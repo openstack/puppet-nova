be9e0e208190d5b469429f24b0491f4b5bd29bed
Aimon Bustardo <abustardo@morphlabs.com>
2012-11-06 15:11:05 -0800

    wrap utilities packages in a if \!defined to prevent conflict with rvm module

--- manifests/utilities.pp
+++ manifests/utilities.pp
@@ -1,8 +1,14 @@
 # unzip swig screen parted curl euca2ools - extra packages
 class nova::utilities {
   if $::osfamily == 'Debian' {
-    package { ['unzip', 'screen', 'parted', 'curl', 'euca2ools']:
-      ensure => present
+    define nova::utilities::install(){
+      if !defined(Package[$title]){
+        package { $title:
+          ensure => present
+        }
+      }
     }
+    $pkgs=['unzip', 'screen', 'parted', 'curl', 'euca2ools']
+    nova::utilities::install{$pkgs:}
   }
 }
