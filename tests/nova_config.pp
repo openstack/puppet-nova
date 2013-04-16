198a0d401b9ff4d5ef8999b550dd76f5ecb8b44d
Dan Bode <bodepd@gmail.com>
2013-04-16 12:33:46 -0700

    remove unused files
    removes the following unused files:
    
    nova.sh - was used for testing. Now I use the test_nova file from
    the openstack modules
    
    tests directory - no longer used, replaced by rspec tests in spec and
    deployment examples in examples
    
    TODO - should not be included, open ticket in launchpad
    
    Change-Id: Ie898d787fd5f38a1bde0b04da39f4786a21bd379

--- tests/nova_config.pp
+++
@@ -1,9 +0,0 @@
-resources { 'nova_config':
-  purge => true
-}
-nova_config { ['DEFAULT/verbose', 'DEFAULT/nodaemomize']:
-  value => 'true',
-}
-nova_config { 'DEFAULT/xenapi_connection_username':
-  value => 'rootty',
-}
