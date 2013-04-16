6c86fa56413a8cbf7d5593b68c2d15114385a516
Mathieu Gagné <mgagne@iweb.com>
2013-04-16 19:42:13 -0400

    Add quantum_region_name parameter
    This parameter configures the region to use with Quantum. It defaults
    to "RegionOne" which is the default used by quantum::keystone::auth.
    
    Change-Id: Ice528825fb2e35016f6fdf68ff5b3beb0ce45d85

--- manifests/network/quantum.pp
+++ manifests/network/quantum.pp
@@ -1,17 +1,44 @@
+# == Class: nova::network::quantum
 #
-# == parameters
-#  * quantum_admin_password: password for quantum keystone user.
-#  * quantum_auth_strategy: auth strategy used by quantum.
-#  * quantum_url
-#  * quantum_admin_tenant_name
-#  * quantum_admin_username
-#  * quantum_admin_auth_url
+# Configures Nova network to use Quantum.
+#
+# === Parameters
+#
+# [*quantum_admin_password*]
+#   (required) Password for connecting to Quantum network services in
+#   admin context through the OpenStack Identity service.
+#
+# [*quantum_auth_strategy*]
+#   (optional) Should be kept as default 'keystone' for all production deployments.
+#
+# [*quantum_url*]
+#   (optional) URL for connecting to the Quantum networking service.
+#   Defaults to 'http://127.0.0.1:9696'.
+#
+# [*quantum_admin_tenant_name*]
+#   (optional) Tenant name for connecting to Quantum network services in
+#   admin context through the OpenStack Identity service. Defaults to 'services'.
+#
+# [*quantum_region_name*]
+#   (optional) Region name for connecting to quantum in admin context
+#   through the OpenStack Identity service. Defaults to 'RegionOne'.
+#
+# [*quantum_admin_username*]
+#   (optional) Username for connecting to Quantum network services in admin context
+#   through the OpenStack Identity service. Defaults to 'quantum'.
+#
+# [*quantum_admin_auth_url*]
+#   (optional) Points to the OpenStack Identity server IP and port.
+#   This is the Identity (keystone) admin API server IP and port value,
+#   and not the Identity service API IP and port.
+#   Defaults to 'http://127.0.0.1:35357/v2.0'
 #
 class nova::network::quantum (
   $quantum_admin_password,
   $quantum_auth_strategy     = 'keystone',
   $quantum_url               = 'http://127.0.0.1:9696',
   $quantum_admin_tenant_name = 'services',
+  $quantum_region_name       = 'RegionOne',
   $quantum_admin_username    = 'quantum',
   $quantum_admin_auth_url    = 'http://127.0.0.1:35357/v2.0'
 ) {
@@ -21,6 +48,7 @@ class nova::network::quantum (
     'DEFAULT/network_api_class':         value => 'nova.network.quantumv2.api.API';
     'DEFAULT/quantum_url':               value => $quantum_url;
     'DEFAULT/quantum_admin_tenant_name': value => $quantum_admin_tenant_name;
+    'DEFAULT/quantum_region_name':       value => $quantum_region_name;
     'DEFAULT/quantum_admin_username':    value => $quantum_admin_username;
     'DEFAULT/quantum_admin_password':    value => $quantum_admin_password;
     'DEFAULT/quantum_admin_auth_url':    value => $quantum_admin_auth_url;
