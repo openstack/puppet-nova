daac3744caef40a4770d467391d270dd28514809
Sandro Mathys <sm@sandro-mathys.ch>
2013-10-17 14:15:22 +0200

    Configure libvirt migration on the RedHat and Debian-Ubuntu
    It introduces Red Hat support and fix Ubuntu / Debian libvirt
    configuration to support live migrations when using libvirt nova driver.
    
    Change-Id: I72f03316e79ce3b83959def79b86056c934ce62d
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- manifests/migration/libvirt.pp
+++ manifests/migration/libvirt.pp
@@ -1,10 +1,8 @@
 #
 class nova::migration::libvirt {
 
-  case $::lsbdistid {
-    'Ubuntu': {
-      # Ubuntu-specific, not Debian, due to upstart
-
+  case $::osfamily {
+    'RedHat': {
       file_line { '/etc/libvirt/libvirtd.conf listen_tls':
         path   => '/etc/libvirt/libvirtd.conf',
         line   => 'listen_tls = 0',
@@ -26,10 +24,33 @@ class nova::migration::libvirt {
         notify => Service['libvirt'],
       }
 
-      file_line { '/etc/init/libvirt-bin.conf libvirtd opts':
-        path  => '/etc/init/libvirt-bin.conf',
-        line  => 'env libvirtd_opts="-d -l"',
-        match => 'env libvirtd_opts=',
+      file_line { '/etc/sysconfig/libvirtd libvirtd args':
+        path  => '/etc/sysconfig/libvirtd',
+        line  => 'LIBVIRTD_ARGS="--listen"',
+        match => 'LIBVIRTD_ARGS=',
+      }
+    }
+
+    'Debian': {
+      file_line { '/etc/libvirt/libvirtd.conf listen_tls':
+        path   => '/etc/libvirt/libvirtd.conf',
+        line   => 'listen_tls = 0',
+        match  => 'listen_tls =',
+        notify => Service['libvirt'],
+      }
+
+      file_line { '/etc/libvirt/libvirtd.conf listen_tcp':
+        path   => '/etc/libvirt/libvirtd.conf',
+        line   => 'listen_tcp = 1',
+        match  => 'listen_tcp =',
+        notify => Service['libvirt'],
+      }
+
+      file_line { '/etc/libvirt/libvirtd.conf auth_tcp':
+        path   => '/etc/libvirt/libvirtd.conf',
+        line   => 'auth_tcp = "none"',
+        match  => 'auth_tcp =',
+        notify => Service['libvirt'],
       }
 
       file_line { '/etc/default/libvirt-bin libvirtd opts':
@@ -37,9 +58,11 @@ class nova::migration::libvirt {
         line  => 'libvirtd_opts="-d -l"',
         match => 'libvirtd_opts=',
       }
+
     }
+
     default:  {
-      warning("Unsupported operating system: ${::lsbdistid}, make sure you are configuring this yourself")
+      warning("Unsupported osfamily: ${::osfamily}, make sure you are configuring this yourself")
     }
   }
 }
