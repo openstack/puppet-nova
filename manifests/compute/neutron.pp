452bf19021fa089147ee17489a9ecae7756a9e6a
Emilien Macchi <emilien.macchi@enovance.com>
2013-12-12 12:37:21 +0100

    Fix wrong vif driver by default
    Some changes have been done in Nova to have generic drivers:
    http://tinyurl.com/VifDriver
    
    This patchs aims to change default driver.
    
    Closes-bug #1259139
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>
    Change-Id: I38ab21b7a3ae0f5b9ec4f08f5e30d2da58381cf1

--- manifests/compute/neutron.pp
+++ manifests/compute/neutron.pp
@@ -4,15 +4,20 @@
 # This will use virtio for VM guests and the
 # specified driver for the VIF
 #
-# === Parameters:
+# === Parameters
 #
 # [*libvirt_vif_driver*]
-#   (optional) The vif driver to configure the VIFs
-#   Defaults to 'nova.virt.libvirt.vif.LibvirtOpenVswitchDriver'
+#   (optional) The libvirt VIF driver to configure the VIFs.
+#   Defaults to 'nova.virt.libvirt.vif.LibvirtGenericVIFDriver'.
 #
+
 class nova::compute::neutron (
-  $libvirt_vif_driver = 'nova.virt.libvirt.vif.LibvirtOpenVswitchDriver'
+  $libvirt_vif_driver = 'nova.virt.libvirt.vif.LibvirtGenericVIFDriver'
 ) {
+
+  if $libvirt_vif_driver == 'nova.virt.libvirt.vif.LibvirtOpenVswitchDriver' {
+    fail('nova.virt.libvirt.vif.LibvirtOpenVswitchDriver as vif_driver is removed from Icehouse')
+  }
 
   nova_config {
     'DEFAULT/libvirt_vif_driver':             value => $libvirt_vif_driver;
