7aaeb33a01e47af44bdadbd2c63b1f98e27740b8
Piotr Lorek <piotr.lorek@netart.pl>
2013-12-24 10:34:29 +0100

    Ceph RBD support for libvirt with cephx authentication

--- manifests/compute/libvirt.pp
+++ manifests/compute/libvirt.pp
@@ -2,7 +2,13 @@
 class nova::compute::libvirt (
   $libvirt_type      = 'kvm',
   $vncserver_listen  = '127.0.0.1',
-  $migration_support = false
+  $migration_support = false,
+  $libvirt_images_type = 'default',
+  $libvirt_images_rbd_pool = 'rbd',
+  $libvirt_images_rbd_ceph_conf = '/etc/ceph/ceph.conf',
+  $rbd_user             = unset,
+  $rbd_secret_uuid      = unset,
+  $rbd_secret_val       = unset,
 ) {
 
   include nova::params
@@ -46,10 +52,55 @@ class nova::compute::libvirt (
     require  => Package['libvirt'],
   }
 
+
+  if ($libvirt_images_type == 'rbd'){
+    if $rbd_user {
+      file { '/etc/libvirt/secret.xml':
+        ensure   => file,
+        owner    => 'root',
+        group    => 'root',
+        mode     => '600',
+        content  => template("${module_name}/secret.xml.erb"),
+        require  => Package['libvirt'],
+        notify   => Exec['secret_refresh'],
+      }
+
+      exec { 'secret_define':
+        path        => '/usr/bin:/usr/sbin:/bin',
+        command     => 'virsh secret-define /etc/libvirt/secret.xml',
+        unless      => "virsh secret-dumpxml ${rbd_secret_uuid}",
+        require     => File['/etc/libvirt/secret.xml'],
+      }
+
+      exec { 'secret_set':
+        path        => '/usr/bin:/usr/sbin:/bin',
+        command     => "virsh secret-set-value ${rbd_secret_uuid} ${rbd_secret_val}",
+        unless      => "virsh secret-get-value ${rbd_secret_uuid}",
+        require     => Exec['secret_define'],
+      }
+
+      exec { 'secret_refresh':
+        path        => '/usr/bin:/usr/sbin:/bin',
+        command     => "virsh secret-set-value ${rbd_secret_uuid} ${rbd_secret_val}",
+        refreshonly => true,
+        require     => Exec['secret_define'],
+      }
+    }
+
+    nova_config {
+      'DEFAULT/libvirt_inject_partition':         value => '-2';
+      'DEFAULT/libvirt_images_rbd_pool':          value => $libvirt_images_rbd_pool;
+      'DEFAULT/libvirt_images_rbd_ceph_conf':     value => $libvirt_images_rbd_ceph_conf;
+      'DEFAULT/rbd_user':                         value => $rbd_user;
+      'DEFAULT/rbd_secret_uuid':                  value => $rbd_secret_uuid;
+    }
+  }
+
   nova_config {
-    'DEFAULT/compute_driver':   value => 'libvirt.LibvirtDriver';
-    'DEFAULT/libvirt_type':     value => $libvirt_type;
-    'DEFAULT/connection_type':  value => 'libvirt';
-    'DEFAULT/vncserver_listen': value => $vncserver_listen;
+    'DEFAULT/compute_driver':           value => 'libvirt.LibvirtDriver';
+    'DEFAULT/libvirt_type':             value => $libvirt_type;
+    'DEFAULT/connection_type':          value => 'libvirt';
+    'DEFAULT/vncserver_listen':         value => $vncserver_listen;
+    'DEFAULT/libvirt_images_type':      value => $libvirt_images_type;
   }
 }
