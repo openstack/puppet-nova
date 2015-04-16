3cba5197cc5c4e7807d5c97d374fb175b47878c7
Emilien Macchi <emilien@redhat.com>
2015-04-15 21:30:59 -0400

    Update Beaker tests
    Update beaker tests to cover more services & be more consistent with
    keystone & openstacklib.
    
    Change-Id: Id995ec7cd141aaeaea464781e3680ba4dcd94b42
    Closes-bug: #1444736

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -16,6 +16,7 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
+    # Install module and dependencies
     hosts.each do |host|
 
       # install git
@@ -25,12 +26,12 @@ RSpec.configure do |c|
       shell('rm -fr /etc/puppet/modules/*')
 
       # install library modules from the forge
-      on host, puppet('module','install', 'puppetlabs-mysql', '--version', '3.2.0'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','dprince/qpid'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','duritong/sysctl'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','stahnma-epel'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','puppetlabs-rabbitmq'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module','install','puppetlabs-mysql'), { :acceptable_exit_codes => 0 }
+      on host, puppet('module','install','dprince/qpid'), { :acceptable_exit_codes => 0 }
+      on host, puppet('module','install','duritong/sysctl'), { :acceptable_exit_codes => 0 }
+      on host, puppet('module','install','puppetlabs-inifile'), { :acceptable_exit_codes => 0 }
+      on host, puppet('module','install','stahnma-epel'), { :acceptable_exit_codes => 0 }
+      on host, puppet('module','install','puppetlabs-rabbitmq'), { :acceptable_exit_codes => 0 }
 
       # install puppet modules from git, use master
       shell('git clone https://git.openstack.org/stackforge/puppet-openstacklib /etc/puppet/modules/openstacklib')
@@ -41,7 +42,7 @@ RSpec.configure do |c|
       # Install the module being tested
       puppet_module_install(:source => proj_root, :module_name => 'nova')
       # List modules installed to help with debugging
-      on hosts[0], puppet('module','list'), { :acceptable_exit_codes => [0,1] }
+      on hosts[0], puppet('module','list'), { :acceptable_exit_codes => 0 }
     end
   end
 end
