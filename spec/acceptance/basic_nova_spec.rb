b20f2fdc9726710efe5fe6adff97764949157551
Sebastien Badia <sbadia@redhat.com>
2015-06-24 18:32:41 +0200

    Fix Beaker CI according recent puppetlabs-rabbitmq change.
    The puppetlabs-rabbitmq module introduce a new feature related to the
    erlang_cookie parameter change (the database must be wiped if the cookie is
    changed), the parameter wipe_db_on_cookie_change is configured to false by
    default.
    
    But we're not setting up a rabbitmq cluster and we're not using pacemaker in
    beaker CI tests, so we just remove the erlang cookie parameter change.
    
    Change-Id: I3381092f5503f4a50ab1636cef829145ef22d230
    Closes-Bug: #1468444

--- spec/acceptance/basic_nova_spec.rb
+++ spec/acceptance/basic_nova_spec.rb
@@ -42,7 +42,6 @@ describe 'basic nova' do
 
       class { '::rabbitmq':
         delete_guest_user => true,
-        erlang_cookie     => 'secrete',
         package_provider  => $package_provider,
       }
 
