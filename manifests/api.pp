aa8cf7ee730555b7ae18fe94d2a53a17ccde9fe5
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-06-24 09:55:39 +0200

    remove openstackocci installation feature
    if occiapi is in enabled_apis, this class would install openstackocci via pip.
    unfortunaltely the last version available via pip only works for essex,
    therefore remove this functionality.
    
    Closes-Bug: #1466464
    Change-Id: Id39ad230d7e5d75b01b3083e7b0f991cd11c111b
    Co-Authored-By: BeneDicere <b.von.st.vieth@fz-juelich.de>

--- manifests/api.pp
+++ manifests/api.pp
@@ -314,26 +314,6 @@ class nova::api(
     }
   }
 
-  if 'occiapi' in $enabled_apis {
-    if !defined(Package['python-pip']) {
-      package { 'python-pip':
-        ensure => latest,
-      }
-    }
-    if !defined(Package['pyssf']) {
-      package { 'pyssf':
-        ensure   => latest,
-        provider => pip,
-        require  => Package['python-pip']
-      }
-    }
-    package { 'openstackocci':
-      ensure   => latest,
-      provider => 'pip',
-      require  => Package['python-pip'],
-    }
-  }
-
   if ($ratelimits != undef) {
     nova_paste_api_ini {
       'filter:ratelimit/paste.filter_factory': value => $ratelimits_factory;
