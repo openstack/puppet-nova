e94fa305e75b79513e5f8f265ad3bc7737712dc1
Colleen Murphy <colleen@gazlene.net>
2015-07-07 14:06:05 -0700

    Correct `puppet module list` check in beaker
    The final check during the prep stage, where `puppet module list` is
    run so that its output can be used for debugging, should run on the
    current host in the loop, not the first host in the array.
    
    Change-Id: I5384fb84c67c10a38cb57850dbf50746a785312c

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -43,7 +43,7 @@ RSpec.configure do |c|
       # Install the module being tested
       puppet_module_install(:source => proj_root, :module_name => 'nova')
       # List modules installed to help with debugging
-      on hosts[0], puppet('module','list'), { :acceptable_exit_codes => 0 }
+      on host, puppet('module','list'), { :acceptable_exit_codes => 0 }
     end
   end
 end
