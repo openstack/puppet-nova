459d075c61a6719aabe2af825153acd3456c7d07
Martin Mágr <mmagr@redhat.com>
2015-07-31 16:14:56 +0200

    Allow to change archive destination
    Adds 'destination' parameter to nova::cron::archive_deleted_rows
    
    Change-Id: Ic674f4d39bc88f89abfeb0ce99a571c2534e57e4

--- spec/classes/nova_cron_archive_deleted_rows_spec.rb
+++ spec/classes/nova_cron_archive_deleted_rows_spec.rb
@@ -6,16 +6,27 @@ describe 'nova::cron::archive_deleted_rows' do
     { :osfamily => 'Debian' }
   end
 
-  it 'configures a cron' do
-    is_expected.to contain_cron('nova-manage db archive_deleted_rows').with(
-      :command     => 'nova-manage db archive_deleted_rows --max_rows 100 >>/var/log/nova/nova-rowsflush.log 2>&1',
-      :environment => 'PATH=/bin:/usr/bin:/usr/sbin SHELL=/bin/sh',
-      :user        => 'nova',
-      :minute      => 1,
+  let :params do
+    { :minute      => 1,
       :hour        => 0,
       :monthday    => '*',
       :month       => '*',
       :weekday     => '*',
+      :max_rows    => '100',
+      :user        => 'nova',
+      :destination => '/var/log/nova/nova-rowsflush.log' }
+  end
+
+  it 'configures a cron' do
+    is_expected.to contain_cron('nova-manage db archive_deleted_rows').with(
+      :command     => "nova-manage db archive_deleted_rows --max_rows #{params[:max_rows]} >>#{params[:destination]} 2>&1",
+      :environment => 'PATH=/bin:/usr/bin:/usr/sbin SHELL=/bin/sh',
+      :user        => params[:user],
+      :minute      => params[:minute],
+      :hour        => params[:hour],
+      :monthday    => params[:monthday],
+      :month       => params[:month],
+      :weekday     => params[:weekday],
       :require     => 'Package[nova-common]',
     )
   end
