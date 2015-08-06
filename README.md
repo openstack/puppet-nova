4e5e9896035ec1e60e5ad5ca223323cf738b1f5a
Yanis Guenane <yguenane@redhat.com>
2015-08-06 13:06:14 +0200

    Reflect provider change in puppet-openstacklib
    With the creation of the new openstack_config provider, some processing
    that was done in nova_config has been centralized in
    openstack_config.
    
    Impacted methods are :
    
      * section
      * setting
      * separator
    
    Also, this commit adds the fact that, when passing a specific string
    (ensure_absent_val) the provider will behave as if ensure => absent was
    specified. '<SERVICE DEFAULT>' is the default value for
    ensure_absent_val.
    
    The use case is the following :
    
    nova_config { 'DEFAULT/foo' : value => 'bar' } # will work as usual
    
    nova_config { 'DEFAULT/foo' : value => '<SERVICE DEFAULT>' } # will mean absent
    
    That means that all the current :
    
    if $myvar {
      nova_config { 'DEFAULT/foo' : value => $myvar }
    } else {
      nova_config { 'DEFAULT/foo' : ensure => absent }
    }
    
    can be removed in favor of :
    
    nova_config { 'DEFAULT/foo' : value => $myvar }
    
    If for any reason '<SERVICE DEFAULT>' turns out to be a valid value for
    a specific parameter. One could by pass that doing the following :
    
    nova_config { 'DEFAULT/foo' : value => '<SERVICE DEFAULT>',
    ensure_absent_val => 'foo' }
    
    Change-Id: I7ba9bf6c07edd7248e401ef5f55abf6e3464a0a3
    Depends-On: I0eeebde3aac2662cc7e69bfad7f8d2481463a218

--- README.md
+++ README.md
@@ -68,6 +68,36 @@ Implementation
 
 nova is a combination of Puppet manifest and ruby code to delivery configuration and extra functionality through types and providers.
 
+### Types
+
+#### nova_config
+
+The `nova_config` provider is a children of the ini_setting provider. It allows one to write an entry in the `/etc/nova/nova.conf` file.
+
+```puppet
+nova_config { 'DEFAULT/verbose' :
+  value => true,
+}
+```
+
+This will write `verbose=true` in the `[DEFAULT]` section.
+
+##### name
+
+Section/setting name to manage from `nova.conf`
+
+##### value
+
+The value of the setting to be defined.
+
+##### secret
+
+Whether to hide the value from Puppet logs. Defaults to `false`.
+
+##### ensure_absent_val
+
+If value is equal to ensure_absent_val then the resource will behave as if `ensure => absent` was specified. Defaults to `<SERVICE DEFAULT>`
+
 Limitations
 -----------
 
--- spec/unit/provider/nova_config/ini_setting_spec.rb
+++ spec/unit/provider/nova_config/ini_setting_spec.rb
@@ -14,6 +14,17 @@ $LOAD_PATH.push(
     'inifile',
     'lib')
 )
+$LOAD_PATH.push(
+  File.join(
+    File.dirname(__FILE__),
+    '..',
+    '..',
+    '..',
+    'fixtures',
+    'modules',
+    'openstacklib',
+    'lib')
+)
 require 'spec_helper'
 provider_class = Puppet::Type.type(:nova_config).provider(:ini_setting)
 describe provider_class do
@@ -35,4 +46,23 @@ describe provider_class do
     expect(provider.section).to eq('dude')
     expect(provider.setting).to eq('foo')
   end
+
+  it 'should ensure absent when <SERVICE DEFAULT> is specified as a value' do
+    resource = Puppet::Type::Nova_config.new(
+      {:name => 'dude/foo', :value => '<SERVICE DEFAULT>'}
+    )
+    provider = provider_class.new(resource)
+    provider.exists?
+    expect(resource[:ensure]).to eq :absent
+  end
+
+  it 'should ensure absent when value matches ensure_absent_val' do
+    resource = Puppet::Type::Nova_config.new(
+      {:name => 'dude/foo', :value => 'foo', :ensure_absent_val => 'foo' }
+    )
+    provider = provider_class.new(resource)
+    provider.exists?
+    expect(resource[:ensure]).to eq :absent
+  end
+
 end
