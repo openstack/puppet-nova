196dce78ad9c416c8e2d0932698fd9c511639ed1
Emilien Macchi <emilien@redhat.com>
2015-08-16 09:50:01 -0400

    acceptance: enable debug & verbosity for OpenStack logs
    Enable debug & verbose when possible for OpenStack services logs, so we can read a maximum
    of informations in Jenkins jobs logs and easily debug our eventual failures.
    
    Change-Id: I62b8510442a747d2f909bdceb0717839c9778e6d

--- spec/acceptance/basic_nova_spec.rb
+++ spec/acceptance/basic_nova_spec.rb
@@ -98,7 +98,8 @@ describe 'basic nova' do
         rabbit_password     => 'an_even_bigger_secret',
         image_service       => 'nova.image.glance.GlanceImageService',
         glance_api_servers  => 'localhost:9292',
-        verbose             => false,
+        verbose             => true,
+        debug               => true,
         rabbit_host         => '127.0.0.1',
       }
       class { '::nova::db::mysql':
