7e7e848170117e2bb31840778e642132bef136e2
Maksym Yatsenko <myatsenko@mirantis.com>
2015-10-09 14:31:31 +0300

    Floating IP range support in Nova network
    This patch provides a possibility to add
    ip ranges of networks (i.e.: 192.168.1.210-192.168.1.224)
    to a floating ip pool.
    
    Depends-On: I802280e4e2912c9aed1c10d519ead84b1f22f900
    
    Change-Id: Iffb0af32753708decb8d2b60f1075b66f7520553

--- lib/puppet/provider/nova_floating/nova_manage.rb
+++ lib/puppet/provider/nova_floating/nova_manage.rb
@@ -1,3 +1,5 @@
+require 'netaddr'
+
 Puppet::Type.type(:nova_floating).provide(:nova_manage) do
 
   desc "Manage nova floating"
@@ -5,19 +7,79 @@ Puppet::Type.type(:nova_floating).provide(:nova_manage) do
   optional_commands :nova_manage => 'nova-manage'
 
   def exists?
-    # Calculate num quads to grab for prefix
-    mask=resource[:network].sub(/.*\/([0-9][0-9]?)/, '\1').to_i
-    num_quads = 4 - mask / 8
-    prefix=resource[:network].sub(/(\.[0-9]{1,3}){#{num_quads}}(\/[0-9]{1,2})?$/, '') + "."
-    return nova_manage("floating", "list").match(/#{prefix}/)
+    if resource[:network] =~ /^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/
+      # Calculate num quads to grab for prefix$$
+      mask=resource[:network].sub(/.*\/([0-9][0-9]?)/, '\1').to_i
+      num_quads = 4 - mask / 8
+      prefix=resource[:network].sub(/(\.[0-9]{1,3}){#{num_quads}}(\/[0-9]{1,2})?$/, '') + "."
+      return nova_manage("floating", "list").match(/#{prefix}/)
+
+    elsif resource[:network].to_s =~ /^.{0,2}\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}-\d{1,3}\.\d{1,3}.\d{1,3}.\d{1,3}.{0,2}/
+      if resource[:ensure] == :absent
+        operate_range.any?
+      else
+        operate_range.empty?
+      end
+    end
   end
 
   def create
-     nova_manage("floating", "create", '--pool', resource[:pool], resource[:network])
+    if resource[:network] =~ /^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/
+      nova_manage("floating", "create", '--pool', resource[:pool], resource[:network])
+
+    elsif resource[:network].to_s =~ /^.{0,2}\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}-\d{1,3}\.\d{1,3}.\d{1,3}.\d{1,3}.{0,2}/
+      mixed_range.each do |ip|
+        nova_manage("floating", "create", ip)
+      end
+    end
   end
 
   def destroy
-    nova_manage("floating", "delete", resource[:network])
+    if resource[:network] =~ /^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/
+      nova_manage("floating", "delete", resource[:network])
+
+    elsif resource[:network].to_s =~ /^.{0,2}\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}-\d{1,3}\.\d{1,3}.\d{1,3}.\d{1,3}.{0,2}/
+      mixed_range.each do |ip|
+        nova_manage("floating", "delete", ip )
+      end
+    end
   end
 
+  # Create range in cidr, including first and last ip
+  # Nova will create this range, excluding network and broadcast IPs
+  def mixed_range
+    range = []
+    NetAddr.merge(operate_range).each do |cidr|
+      tmp_range = NetAddr::CIDR.create(cidr).enumerate
+      range << tmp_range.first.to_s
+      range << tmp_range.last.to_s
+    end
+    range.uniq!
+    range += NetAddr.merge(operate_range).delete_if{ |part| part =~ /\/3[12]/}
+  end
+
+  # Calculate existed IPs and provided ip ranges
+  def operate_range
+    exist_range = []
+    output = nova_manage("floating", "list")
+    range_list = output.scan(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
+    range_list.each do |element|
+      exist_range << element
+    end
+    if @resource[:ensure] == :absent
+       ip_range & exist_range
+    else
+       ip_range - exist_range
+    end
+  end
+
+  def ip_range
+    ip_range = []
+    Array(@resource[:network]).each do |rng|
+      ip = rng.split('-')
+      generated_range = NetAddr.range(NetAddr::CIDR.create(ip.first), NetAddr::CIDR.create(ip.last))
+      ip_range += generated_range.unshift(ip.first).push(ip.last)
+    end
+    return ip_range
+  end
 end
--- lib/puppet/type/nova_floating.rb
+++ lib/puppet/type/nova_floating.rb
@@ -5,8 +5,9 @@ Puppet::Type.newtype(:nova_floating) do
   ensurable
 
   newparam(:network, :namevar => true) do
-    desc "Network (ie, 192.168.1.0/24 or 192.168.1.128/25 etc.)"  
-    newvalues(/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/)
+    desc "It can contain network (ie, 192.168.1.0/24 or 192.168.1.128/25 etc.),
+          ip range ('192.168.1.1-192.168.1.55' or list of ip ranges ['192.168.1.1-192.168.1.25', '192.168.1.30-192.168.1.55'])"
+    newvalues(/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$ || ^(\d{1,3}\.){3}\d{1,3}-(\d{1,3}\.){3}\d{1,3}$/)
   end
 
   newparam(:pool) do
