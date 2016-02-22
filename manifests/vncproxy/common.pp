mbaldessari/openstack-puppet-modules

Supports IPv6 in vnc proxy.
This patch add support for IPv6 address in vnc proxy url.  It adds
brackets if a ipv6 is detected.

Change-Id: I12fb8f01b5bdca2bfea856c8a5e158fa1d87a15b
Related-Bug: 1531960
Depends-On: I093dd5a4e6294e20761cb3d33373652eeadeac36

nova/manifests/vncproxy/common.pp
@@ -23,11 +23,11 @@ class nova::vncproxy::common (
   $vncproxy_path     = undef,
 ) {
 
-  $vncproxy_host_real     = pick(
+  $vncproxy_host_real     = ipv6_add_bracket_maybe(pick(
     $vncproxy_host,
     $::nova::compute::vncproxy_host,
     $::nova::vncproxy::host,
-    false)
+    false))
   $vncproxy_protocol_real = pick(
     $vncproxy_protocol,
     $::nova::compute::vncproxy_protocol,
@@ -43,7 +43,6 @@ class nova::vncproxy::common (
     $::nova::compute::vncproxy_path,
     $::nova::vncproxy::vncproxy_path,
     '/vnc_auto.html')
-
   if ($vncproxy_host_real) {
     $vncproxy_base_url = "${vncproxy_protocol_real}://${vncproxy_host_real}:${vncproxy_port_real}${vncproxy_path_real}"
     # config for vnc proxy

openstack/puppet-nova

brackets if an IPv6 is detected.
(cherry picked from commit 9eafb7d859a2ec36d5c1cc25233ff54ccc5fdfcb)

manifests/vncproxy/common.pp
@@ -23,11 +23,11 @@ class nova::vncproxy::common (
   $vncproxy_path     = undef,
 ) {
 
-  $vncproxy_host_real     = pick(
+  $vncproxy_host_real     = normalize_ip_for_uri(pick(
     $vncproxy_host,
     $::nova::compute::vncproxy_host,
     $::nova::vncproxy::host,
-    false)
+    false))
   $vncproxy_protocol_real = pick(
     $vncproxy_protocol,
     $::nova::compute::vncproxy_protocol,