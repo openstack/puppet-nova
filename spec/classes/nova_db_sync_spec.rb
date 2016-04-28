e04d996d371edb5680295a39b5ee43e2172befd2
ZhongShengping <chdzsp@163.com>
2016-04-29 00:10:15 +0800

    Using on_support_os function to test
    Change-Id: I7148c304f9ac3d272ae3e201c5ce021bac2ff5d8

--- spec/classes/nova_db_sync_spec.rb
+++ spec/classes/nova_db_sync_spec.rb
@@ -31,29 +31,19 @@ describe 'nova::db::sync' do
   end
 
 
-  context 'on a RedHat osfamily' do
-    let :facts do
-     @default_facts.merge({
-        :osfamily                 => 'RedHat',
-        :operatingsystemrelease   => '7.0',
-        :concat_basedir => '/var/lib/puppet/concat'
-      })
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge(OSDefaults.get_facts({
+          :processorcount => 8,
+          :concat_basedir => '/var/lib/puppet/concat'
+        }))
+      end
+
+      it_configures 'nova-dbsync'
     end
-
-    it_configures 'nova-dbsync'
-  end
-
-  context 'on a Debian osfamily' do
-    let :facts do
-      @default_facts.merge({
-        :operatingsystemrelease => '7.8',
-        :operatingsystem        => 'Debian',
-        :osfamily               => 'Debian',
-        :concat_basedir => '/var/lib/puppet/concat'
-      })
-    end
-
-    it_configures 'nova-dbsync'
   end
 
 end
