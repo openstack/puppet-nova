189a662c869afbe918c8a80ace5f7478d766dc6a
ZhongShengping <chdzsp@163.com>
2016-04-30 11:49:23 +0800

    Test multiple operating systems for db
    This change updates the tests for nova::db::postgresql and
    nova::db::postgresql::api to test multiple operating systems.
    
    Change-Id: I7c83e8ee38fe75703ccfdd73ffbb076bbb1c02e6

--- spec/classes/nova_db_postgresql_api_spec.rb
+++ spec/classes/nova_db_postgresql_api_spec.rb
@@ -2,21 +2,13 @@ require 'spec_helper'
 
 describe 'nova::db::postgresql_api' do
 
-  let :req_params do
-    { :password => 'pw' }
-  end
+  shared_examples_for 'nova::db::postgresql' do
+    let :req_params do
+      { :password => 'pw' }
+    end
 
-  let :pre_condition do
-    'include postgresql::server'
-  end
-
-  context 'on a RedHat osfamily' do
-    let :facts do
-      @default_facts.merge({
-        :osfamily                 => 'RedHat',
-        :operatingsystemrelease   => '7.0',
-        :concat_basedir => '/var/lib/puppet/concat'
-      })
+    let :pre_condition do
+      'include postgresql::server'
     end
 
     context 'with only required parameters' do
@@ -32,27 +24,19 @@ describe 'nova::db::postgresql_api' do
 
   end
 
-  context 'on a Debian osfamily' do
-    let :facts do
-      @default_facts.merge({
-        :operatingsystemrelease => '7.8',
-        :operatingsystem        => 'Debian',
-        :osfamily               => 'Debian',
-        :concat_basedir => '/var/lib/puppet/concat'
-      })
-    end
-
-    context 'with only required parameters' do
-      let :params do
-        req_params
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge(OSDefaults.get_facts({
+          :processorcount => 8,
+          :concat_basedir => '/var/lib/puppet/concat'
+        }))
       end
 
-      it { is_expected.to contain_postgresql__server__db('nova_api').with(
-        :user     => 'nova_api',
-        :password => 'md581802bf81b206888b50950e640d70549'
-      )}
+      it_configures 'nova::db::postgresql'
     end
-
   end
 
 end
