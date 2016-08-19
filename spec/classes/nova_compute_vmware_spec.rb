5075d8b7e8d8d13804503101d0999c499307f2af
Iury Gregory Melo Ferreira <iurygregory@gmail.com>
2016-08-19 01:23:15 -0300

    Fix vmware manifest
    -Add missing attributes
    -Fix section name
    
    Change-Id: I4f5b7530ae9b725241397ef546ad455243f5f5b7
    Closes-bug: #1614646

--- spec/classes/nova_compute_vmware_spec.rb
+++ spec/classes/nova_compute_vmware_spec.rb
@@ -15,26 +15,36 @@ describe 'nova::compute::vmware' do
      :task_poll_interval => 10.5,
      :use_linked_clone => false,
      :wsdl_location => 'http://127.0.0.1:8080/vmware/SDK/wsdl/vim25/vimService.wsdl',
-     :compute_driver => 'vmwareapi.FoobarDriver' }
+     :compute_driver => 'vmwareapi.FoobarDriver',
+     :insecure => true,
+     :datastore_regex => '/(?:[^:]|:[^:])+/' }
   end
 
-  it 'configures vmwareapi in nova.conf' do
-    is_expected.to contain_nova_config('DEFAULT/compute_driver').with_value('vmwareapi.VMwareVCDriver')
-    is_expected.to contain_nova_config('VMWARE/host_ip').with_value(params[:host_ip])
-    is_expected.to contain_nova_config('VMWARE/host_username').with_value(params[:host_username])
-    is_expected.to contain_nova_config('VMWARE/host_password').with_value(params[:host_password])
-    is_expected.to contain_nova_config('VMWARE/cluster_name').with_value(params[:cluster_name])
-    is_expected.to contain_nova_config('VMWARE/api_retry_count').with_value(5)
-    is_expected.to contain_nova_config('VMWARE/maximum_objects').with_value(100)
-    is_expected.to contain_nova_config('VMWARE/task_poll_interval').with_value(5.0)
-    is_expected.to contain_nova_config('VMWARE/use_linked_clone').with_value(true)
-    is_expected.to_not contain_nova_config('VMWARE/wsdl_location')
-  end
+  shared_examples_for 'vmware api' do
 
-  it 'installs suds python package' do
-    is_expected.to contain_package('python-suds').with(
+    context 'with default parameters' do
+      it 'configures vmwareapi in nova.conf' do
+        is_expected.to contain_nova_config('DEFAULT/compute_driver').with_value('vmwareapi.VMwareVCDriver')
+        is_expected.to contain_nova_config('vmware/host_ip').with_value(params[:host_ip])
+        is_expected.to contain_nova_config('vmware/host_username').with_value(params[:host_username])
+        is_expected.to contain_nova_config('vmware/host_password').with_value(params[:host_password])
+        is_expected.to contain_nova_config('vmware/cluster_name').with_value(params[:cluster_name])
+        is_expected.to contain_nova_config('vmware/api_retry_count').with_value(5)
+        is_expected.to contain_nova_config('vmware/maximum_objects').with_value(100)
+        is_expected.to contain_nova_config('vmware/task_poll_interval').with_value(5.0)
+        is_expected.to contain_nova_config('vmware/use_linked_clone').with_value(true)
+        is_expected.to contain_nova_config('vmware/wsdl_location').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_nova_config('vmware/insecure').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_nova_config('vmware/ca_file').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_nova_config('vmware/datastore_regex').with_value('<SERVICE DEFAULT>')
+      end
+
+      it 'installs suds python package' do
+      is_expected.to contain_package('python-suds').with(
                :ensure => 'present'
                 )
+      end
+    end
   end
 
   context 'with optional parameters' do
@@ -44,11 +54,26 @@ describe 'nova::compute::vmware' do
 
     it 'configures vmwareapi in nova.conf' do
       is_expected.to contain_nova_config('DEFAULT/compute_driver').with_value(params[:compute_driver])
-      is_expected.to contain_nova_config('VMWARE/api_retry_count').with_value(params[:api_retry_count])
-      is_expected.to contain_nova_config('VMWARE/maximum_objects').with_value(params[:maximum_objects])
-      is_expected.to contain_nova_config('VMWARE/task_poll_interval').with_value(params[:task_poll_interval])
-      is_expected.to contain_nova_config('VMWARE/use_linked_clone').with_value(false)
-      is_expected.to contain_nova_config('VMWARE/wsdl_location').with_value(params[:wsdl_location])
+      is_expected.to contain_nova_config('vmware/api_retry_count').with_value(params[:api_retry_count])
+      is_expected.to contain_nova_config('vmware/maximum_objects').with_value(params[:maximum_objects])
+      is_expected.to contain_nova_config('vmware/task_poll_interval').with_value(params[:task_poll_interval])
+      is_expected.to contain_nova_config('vmware/use_linked_clone').with_value(false)
+      is_expected.to contain_nova_config('vmware/wsdl_location').with_value(params[:wsdl_location])
+      is_expected.to contain_nova_config('vmware/insecure').with_value(params[:insecure])
+      is_expected.to contain_nova_config('vmware/datastore_regex').with_value(params[:datastore_regex])
     end
   end
+
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os, facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_configures 'vmware api'
+    end
+  end
+
 end
