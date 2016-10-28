87fb09cc6926bd39324f59c2b0b84ba193ddef62
Alex Schultz <aschultz@redhat.com>
2016-10-28 17:28:20 -0600

    Update ironic auth settings
    The underlying ironic auth paramters for nova.conf have been changed.
    nova::ironic::common::admin_username,
    nova::ironic::common::admin_password,
    nova::ironic::common::admin_url,
    nova::ironic::common::admin_tenant_name
    have been deprecated in favor of nova::ironic::common::username,
    nova::ironic::common::password, nova::ironic::common::auth_url,
    nova::ironic::common::project_name.
    
    Also the deprecated options from nova::compute::ironic have been
    removed. Please use the options available via nova::ironic::common.
    
    Change-Id: I163d99c407ef9e0725fec492be6440de67839650
    Co-Authored-By: Xing Zhou <xing.zhou@easystack.cn>

--- manifests/compute/ironic.pp
+++ manifests/compute/ironic.pp
@@ -4,35 +4,9 @@
 #
 # === Parameters:
 #
-# [*admin_username*]
-#   The admin username for Ironic to connect to Nova.
-#   Defaults to 'admin'
-#
-# [*admin_password*]
-#   The admin password for Ironic to connect to Nova.
-#   Defaults to 'ironic'
-#
-# [*admin_url*]
-#   The address of the Keystone api endpoint.
-#   Defaults to 'http://127.0.0.1:35357/v2.0'
-#
-# [*admin_tenant_name*]
-#   The Ironic Keystone tenant name.
-#   Defaults to 'services'
-#
-# [*api_endpoint*]
-#   The url for Ironic api endpoint.
-#   Defaults to 'http://127.0.0.1:6385/v1'
-#
 # [*compute_driver*]
 #   (optional) Compute driver.
 #   Defaults to 'ironic.IronicDriver'
-#
-# [*admin_user*]
-#   (optional) DEPRECATED: Use admin_username instead.
-#
-# [*admin_passwd*]
-#   (optional) DEPRECATED: Use admin_password instead.
 #
 # [*max_concurrent_builds*]
 #   (optional) Maximum number of instance builds to run concurrently
@@ -40,48 +14,11 @@
 #
 class nova::compute::ironic (
   $max_concurrent_builds = $::os_service_default,
-  # DEPRECATED PARAMETERS
-  $admin_username        = undef,
-  $admin_password        = undef,
-  $admin_url             = undef,
-  $admin_tenant_name     = undef,
-  $api_endpoint          = undef,
-  $admin_user            = undef,
-  $admin_passwd          = undef,
   $compute_driver        = 'ironic.IronicDriver'
 ) {
 
   include ::nova::deps
-
-  if $admin_user {
-    warning('The admin_user parameter in class nova::compute::ironic is deprecated, use class nova::ironic::common instead.')
-  }
-
-  if $admin_passwd {
-    warning('The admin_passwd parameter in class nova::compute::ironic is deprecated, use class nova::ironic::common instead.')
-  }
-
-  if $admin_username {
-    warning('The admin_username parameter in class nova::compute::ironic is deprecated, use class nova::ironic::common instead.')
-  }
-
-  if $admin_password {
-    warning('The admin_password parameter in class nova::compute::ironic is deprecated, use class nova::ironic::common instead.')
-  }
-
-  if $admin_url {
-    warning('The admin_url parameter in class nova::compute::ironic is deprecated, use class nova::ironic::common instead.')
-  }
-
-  if $admin_tenant_name {
-    warning('The admin_tenant_name parameter in class nova::compute::ironic is deprecated, use class nova::ironic::common instead.')
-  }
-
-  if $api_endpoint {
-    warning('The api_endpoint parameter in class nova::compute::ironic is deprecated, use class nova::ironic::common instead.')
-  }
-
-  include ::nova::ironic::common
+  require ::nova::ironic::common
 
   nova_config {
     'DEFAULT/compute_driver':           value => $compute_driver;
--- manifests/ironic/common.pp
+++ manifests/ironic/common.pp
@@ -1,5 +1,31 @@
 # == Class: nova::ironic::common
 #
+# [*api_endpoint*]
+#   The url for Ironic api endpoint.
+#   Defaults to 'http://127.0.0.1:6385/v1'
+#
+# [*auth_plugin*]
+#   The authentication plugin to use when connecting to nova.
+#   Defaults to 'password'
+#
+# [*auth_url*]
+#   The address of the Keystone api endpoint.
+#   Defaults to 'http://127.0.0.1:35357/'
+#
+# [*project_name*]
+#   The Ironic Keystone project name.
+#   Defaults to 'services'
+#
+# [*password*]
+#   The admin password for Ironic to connect to Nova.
+#   Defaults to 'ironic'
+#
+# [*username*]
+#   The admin username for Ironic to connect to Nova.
+#   Defaults to 'admin'
+#
+# === DEPRECATED
+#
 # [*admin_username*]
 #   The admin username for Ironic to connect to Nova.
 #   Defaults to 'admin'
@@ -16,43 +42,60 @@
 #   The Ironic Keystone tenant name.
 #   Defaults to 'services'
 #
-# [*api_endpoint*]
-#   The url for Ironic api endpoint.
-#   Defaults to 'http://127.0.0.1:6385/v1'
-#
 class nova::ironic::common (
-  $admin_username    = 'admin',
-  $admin_password    = 'ironic',
-  $admin_tenant_name = 'services',
-  $admin_url         = 'http://127.0.0.1:35357/v2.0',
   $api_endpoint      = 'http://127.0.0.1:6385/v1',
+  $auth_plugin       = 'password',
+  $auth_url          = 'http://127.0.0.1:35357/',
+  $password          = 'ironic',
+  $project_name      = 'services',
+  $username          = 'admin',
+  # DEPRECATED
+  $admin_username    = undef,
+  $admin_password    = undef,
+  $admin_tenant_name = undef,
+  $admin_url         = undef,
 ) {
 
   include ::nova::deps
 
-  $admin_username_real = pick(
-    $::nova::compute::ironic::admin_user,
-    $::nova::compute::ironic::admin_username,
-    $admin_username)
-  $admin_password_real = pick(
-    $::nova::compute::ironic::admin_passwd,
-    $::nova::compute::ironic::admin_password,
-    $admin_password)
-  $admin_tenant_name_real = pick(
-    $::nova::compute::ironic::admin_tenant_name,
-    $admin_tenant_name)
-  $admin_url_real = pick(
-    $::nova::compute::ironic::admin_url,
-    $admin_url)
-  $api_endpoint_real = pick(
-    $::nova::compute::ironic::api_endpoint,
-    $api_endpoint)
+  if ($admin_username) {
+    warning('nova::ironic::common::admin_username is deprecated. Please use username')
+  }
+
+  if ($admin_password) {
+    warning('nova::ironic::common::admin_password is deprecated. Please use password')
+  }
+
+  if ($admin_tenant_name) {
+    warning('nova::ironic::common::admin_tenant_name is deprecated. Please use project_name')
+  }
+
+  if ($admin_url) {
+    warning('nova::ironic::common::admin_url is deprecated. Please use auth_url')
+  }
+
+
+
+  $username_real = pick($admin_username, $username)
+  $password_real = pick($admin_password, $password)
+  $auth_url_real = pick($admin_url, $auth_url)
+  $project_name_real = pick($admin_tenant_name, $project_name)
+
 
   nova_config {
-    'ironic/admin_username':    value => $admin_username_real;
-    'ironic/admin_password':    value => $admin_password_real;
-    'ironic/admin_url':         value => $admin_url_real;
-    'ironic/admin_tenant_name': value => $admin_tenant_name_real;
-    'ironic/api_endpoint':      value => $api_endpoint_real;
+    'ironic/auth_plugin':  value => $auth_plugin;
+    'ironic/username':     value => $username_real;
+    'ironic/password':     value => $password_real;
+    'ironic/auth_url':     value => $auth_url_real;
+    'ironic/project_name': value => $project_name_real;
+    'ironic/api_endpoint': value => $api_endpoint;
+  }
+
+  # TODO(aschultz): these are deprecated, remove in P
+  nova_config {
+    'ironic/admin_username':    value => $username_real;
+    'ironic/admin_password':    value => $password_real;
+    'ironic/admin_url':         value => $auth_url_real;
+    'ironic/admin_tenant_name': value => $project_name_real;
   }
 }
