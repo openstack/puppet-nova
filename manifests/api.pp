9dde5a4108df2448bfd43010112d313f1a73395b
Rajesh Tailor <ratailor@redhat.com>
2017-06-09 15:02:14 +0530

    Fix invalid PCI alias definition
    The PCI alias config type is changed in recent release from
    'oslo_config.ListOpt' to 'oslo_config.MultiStrOpt'.
    
    Here nova expects a multi valued option instead of a list.
    
    Change-Id: Ie27dbbc510c73c685b239a9be4af2700a0eb42f0
    Closes-Bug: #1696955

--- manifests/api.pp
+++ manifests/api.pp
@@ -534,7 +534,7 @@ as a standalone service, or httpd for being run by a httpd server")
 
   if $pci_alias {
     nova_config {
-      'pci/pci_alias': value => check_array_of_hash($pci_alias);
+      'pci/pci_alias': value => join(any2array(check_array_of_hash($pci_alias)), ',');
     }
   }
 
