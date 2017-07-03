e869005e6abf7dfa602f81a00059672ae87ea2c5
Yaguang Tang <tangyg2@lenovo.com>
2017-07-03 21:58:49 +0800

    Remove deprecated keystone authtoken signing_dir option
    Change-Id: I3f8093508d12abb640711e79301b301eb2e9de96

--- manifests/keystone/authtoken.pp
+++ manifests/keystone/authtoken.pp
@@ -185,11 +185,6 @@
 #   (in seconds). Set to -1 to disable caching completely. Integer value
 #   Defaults to $::os_service_default.
 #
-# DEPRECATED PARAMETERS
-#
-# [*signing_dir*]
-#   (Optional) Directory used to cache files related to PKI tokens.
-#   Defaults to undef
 #
 class nova::keystone::authtoken(
   $username                       = 'nova',
@@ -227,17 +222,12 @@ class nova::keystone::authtoken(
   $region_name                    = $::os_service_default,
   $revocation_cache_time          = $::os_service_default,
   $token_cache_time               = $::os_service_default,
-  # DEPRECATED PARAMETERS
-  $signing_dir                    = undef,
 ) {
 
   if is_service_default($password) {
     fail('Please set password for nova service user')
   }
 
-  if $signing_dir {
-    warning('signing_dir parameter is deprecated, has no effect and will be removed in the P release.')
-  }
 
   keystone::resource::authtoken { 'nova_config':
     username                       => $username,
