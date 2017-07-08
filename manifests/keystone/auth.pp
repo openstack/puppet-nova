fd80d3812daaa003baf88fb437a22d73e5ca15f3
Yaguang Tang <tangyg2@lenovo.com>
2017-07-08 18:09:09 +0800

    Remove Nova API v3 endpoint auth options
    This can be safely removed after Newton release, and Nova already has
    no support for V3 API.
    
    Change-Id: Ic91f3b2e29a880be3ba32868d1e5e2d370449020

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -55,36 +55,6 @@
 #   (optional) Whether to configure the admin role for the service user.
 #   Defaults to true
 #
-# DEPRECATED PARAMETERS
-#
-# [*public_url_v3*]
-#   (optional) Deprecated. The v3 endpoint's public url.
-#   Defaults to undef.
-#
-# [*internal_url_v3*]
-#   (optional) Deprecated. The v3 endpoint's internal url.
-#   Defaults to undef.
-#
-# [*admin_url_v3*]
-#   (optional) DEPRECATED The v3 endpoint's admin url.
-#   Defaults to undef.
-#
-# [*service_description_v3*]
-#   (optional) Deprecated. Description for keystone v3 service.
-#   Defaults to undef.
-#
-# [*service_name_v3*]
-#   (optional) Deprecated. Name of the v3 service.
-#   Defaults to undef.
-#
-# [*auth_name_v3*]
-#   (optional) Deprecated. The name of the nova v3 service user
-#   Defaults to undef.
-#
-# [*configure_endpoint_v3*]
-#   (optional) Deprecated. Whether to create the v3 endpoint.
-#   Defaults to undef.
-#
 class nova::keystone::auth(
   $password,
   $auth_name               = 'nova',
@@ -99,24 +69,10 @@ class nova::keystone::auth(
   $configure_endpoint      = true,
   $configure_user          = true,
   $configure_user_role     = true,
-  # DEPRECATED PARAMETERS
-  $auth_name_v3            = undef,
-  $service_description_v3  = undef,
-  $service_name_v3         = undef,
-  $public_url_v3           = undef,
-  $internal_url_v3         = undef,
-  $admin_url_v3            = undef,
-  $configure_endpoint_v3   = undef,
 ) {
 
   include ::nova::deps
 
-  if $auth_name_v3 or $service_description_v3 or $service_name_v3 or
-      $public_url_v3 or $internal_url_v3 or $admin_url_v3 or $configure_endpoint_v3 {
-
-    warning("all parameters related to v3 API in nova::keystone::auth are \
-deprecated, have no effect and will be removed after Newton release.")
-  }
 
   if $configure_endpoint {
     Keystone_endpoint["${region}/${service_name}::compute"] ~> Service <| name == 'nova-api' |>
