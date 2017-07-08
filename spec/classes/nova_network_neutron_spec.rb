1e49acf26de1f21e6c878a9a30131376bc50b07e
Yaguang Tang <tangyg2@lenovo.com>
2017-07-09 00:11:04 +0800

    Removes deprecated neutron auth options and tests
    Change-Id: Ib9ae12347999d6d566418522765610c8a3496727

--- spec/classes/nova_network_neutron_spec.rb
+++ spec/classes/nova_network_neutron_spec.rb
@@ -41,7 +41,6 @@ describe 'nova::network::neutron' do
       is_expected.to contain_nova_config('neutron/user_domain_name').with_value(default_params[:neutron_user_domain_name])
       is_expected.to contain_nova_config('neutron/auth_url').with_value(default_params[:neutron_auth_url])
       is_expected.to contain_nova_config('neutron/extension_sync_interval').with_value(default_params[:neutron_extension_sync_interval])
-      is_expected.to contain_nova_config('neutron/default_tenant_id').with_ensure('absent')
     end
     it 'configures Nova to use Neutron Bridge Security Groups and Firewall' do
       is_expected.to contain_nova_config('DEFAULT/firewall_driver').with_value(default_params[:firewall_driver])
@@ -75,7 +74,6 @@ describe 'nova::network::neutron' do
     end
 
     it 'configures neutron endpoint in nova.conf' do
-      is_expected.to contain_nova_config('neutron/auth_strategy').with_ensure('absent')
       is_expected.to contain_nova_config('neutron/password').with_value(params[:neutron_password]).with_secret(true)
       is_expected.to contain_nova_config('DEFAULT/dhcp_domain').with_value(params[:dhcp_domain])
       is_expected.to contain_nova_config('neutron/default_floating_pool').with_value(params[:default_floating_pool])
@@ -88,7 +86,6 @@ describe 'nova::network::neutron' do
       is_expected.to contain_nova_config('neutron/user_domain_name').with_value(params[:neutron_user_domain_name])
       is_expected.to contain_nova_config('neutron/auth_url').with_value(params[:neutron_auth_url])
       is_expected.to contain_nova_config('neutron/extension_sync_interval').with_value(params[:neutron_extension_sync_interval])
-      is_expected.to contain_nova_config('neutron/default_tenant_id').with_ensure('absent')
     end
     it 'configures Nova to use Neutron Security Groups and Firewall' do
       is_expected.to contain_nova_config('DEFAULT/firewall_driver').with_value(params[:firewall_driver])
@@ -105,34 +102,23 @@ describe 'nova::network::neutron' do
       params.merge!(
         :neutron_url                     => 'http://10.0.0.1:9696',
         :neutron_url_timeout             => '30',
-        :neutron_admin_tenant_name       => 'openstack',
-        :neutron_default_tenant_id       => 'default',
         :neutron_region_name             => 'RegionTwo',
-        :neutron_admin_username          => 'neutron2',
-        :neutron_admin_auth_url          => 'http://10.0.0.1:35357',
         :firewall_driver                 => 'nova.virt.firewall.IptablesFirewallDriver',
         :neutron_ovs_bridge              => 'br-int',
         :neutron_extension_sync_interval => '600',
         :vif_plugging_is_fatal           => false,
         :vif_plugging_timeout            => '0',
         :dhcp_domain                     => 'foo',
-        :neutron_auth_plugin             => 'oldparam',
       )
     end
 
     it 'configures neutron endpoint in nova.conf' do
-      is_expected.to contain_nova_config('neutron/auth_strategy').with_ensure('absent')
       is_expected.to contain_nova_config('neutron/password').with_value(params[:neutron_password]).with_secret(true)
       is_expected.to contain_nova_config('DEFAULT/dhcp_domain').with_value(params[:dhcp_domain])
       is_expected.to contain_nova_config('neutron/url').with_value(params[:neutron_url])
       is_expected.to contain_nova_config('neutron/timeout').with_value(params[:neutron_url_timeout])
-      is_expected.to contain_nova_config('neutron/project_name').with_value(params[:neutron_admin_tenant_name])
-      is_expected.to contain_nova_config('neutron/default_tenant_id').with_value(params[:neutron_default_tenant_id])
       is_expected.to contain_nova_config('neutron/region_name').with_value(params[:neutron_region_name])
-      is_expected.to contain_nova_config('neutron/username').with_value(params[:neutron_admin_username])
-      is_expected.to contain_nova_config('neutron/auth_url').with_value(params[:neutron_admin_auth_url])
       is_expected.to contain_nova_config('neutron/extension_sync_interval').with_value(params[:neutron_extension_sync_interval])
-      is_expected.to contain_nova_config('neutron/auth_type').with_value(params[:neutron_auth_plugin])
     end
     it 'configures Nova to use Neutron Security Groups and Firewall' do
       is_expected.to contain_nova_config('DEFAULT/firewall_driver').with_value(params[:firewall_driver])
