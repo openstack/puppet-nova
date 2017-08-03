fbff847ef6e6c8f3c79da0a771baee007c207748
ZhongShengping <chdzsp@163.com>
2017-08-03 10:26:16 +0800

    Fix unit test for nova::client class
    Change-Id: Iaf004263507a9b5f6df80ec2d374dadc17efb463

--- spec/classes/nova_client_spec.rb
+++ spec/classes/nova_client_spec.rb
@@ -2,19 +2,28 @@ require 'spec_helper'
 
 describe 'nova::client' do
 
-  context 'with default parameters' do
-    it {
+  shared_examples_for 'nova client' do
+
+    it { is_expected.to contain_class('nova::deps') }
+
+    it 'installs nova client package' do
       is_expected.to contain_package('python-novaclient').with(
         :ensure => 'present',
         :tag    => ['openstack', 'nova-support-package']
       )
-    }
+    end
   end
 
-  context 'with ensure parameter provided' do
-    let :params do
-      { :ensure => '2012.1-2' }
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_behaves_like 'nova client'
     end
-    it { is_expected.to contain_package('python-novaclient').with_ensure('2012.1-2') }
   end
+
 end
