30edd5b793651135d6729c007f82ab020132b747
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2017-10-03 14:23:32 +0300

    Allow setting log files for nova-placement apache vhost
    This allows the setting of the error and access file logs, as well as
    the access log format. This was done in a similar fashion as one can
    configure these ones in the keystone wsgi manifest.
    
    Change-Id: I59d02fe8731c20c64ca389000f12c78cfc1f12be

--- manifests/wsgi/apache_placement.pp
+++ manifests/wsgi/apache_placement.pp
@@ -72,6 +72,18 @@
 #     apache::vhost ssl parameters.
 #     Optional. Default to apache::vhost 'ssl_*' defaults.
 #
+#   [*access_log_file*]
+#     The log file name for the virtualhost.
+#     Optional. Defaults to false.
+#
+#   [*access_log_format*]
+#     The log format for the virtualhost.
+#     Optional. Defaults to false.
+#
+#   [*error_log_file*]
+#     The error log file name for the virtualhost.
+#     Optional. Defaults to undef.
+#
 #   [*custom_wsgi_process_options*]
 #     (optional) gives you the oportunity to add custom process options or to
 #     overwrite the default options for the WSGI main process.
@@ -104,6 +116,9 @@ class nova::wsgi::apache_placement (
   $threads                     = $::os_workers,
   $priority                    = '10',
   $ensure_package              = 'present',
+  $access_log_file             = false,
+  $access_log_format           = false,
+  $error_log_file              = undef,
   $custom_wsgi_process_options = {},
 ) {
 
@@ -160,6 +175,9 @@ class nova::wsgi::apache_placement (
     wsgi_script_file            => 'nova-placement-api',
     wsgi_script_source          => $::nova::params::placement_wsgi_script_source,
     custom_wsgi_process_options => $custom_wsgi_process_options,
+    access_log_file             => $access_log_file,
+    access_log_format           => $access_log_format,
+    error_log_file              => $error_log_file,
   }
 
 }
