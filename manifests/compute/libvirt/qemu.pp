b9327059921a74963bd9be7452b411b2924f4f04
Saravanan KR <skramaja@redhat.com>
2017-11-28 19:00:52 +0530

    Add group configuration to qemu.conf file
    Vhost user sockets are shared between qemu and ovs. Currently,
    we are patching ovs service file to make ovs run as qemu user.
    In order to avoid it, a new group 'hugetlbfs' has been introduced,
    which will be used by ovs and qemu. This patch allows the users
    to configure the group setting for qemu.conf file.
    
    Change-Id: I0fa3934b345d51de85fd9d8a5a211b8e1bc50d86
    (cherry picked from commit 87e9b300045bb708baf1f0216e6261470de3f04f)

--- manifests/compute/libvirt/qemu.pp
+++ manifests/compute/libvirt/qemu.pp
@@ -8,6 +8,10 @@
 #   (optional) Whether or not configure qemu bits.
 #   Defaults to false.
 #
+# [*group*]
+#   (optional) Group under which the qemu should run.
+#   Defaults to undef.
+#
 # [*max_files*]
 #   (optional) Maximum number of opened files, per process.
 #   Defaults to 1024.
@@ -18,6 +22,7 @@
 #
 class nova::compute::libvirt::qemu(
   $configure_qemu = false,
+  $group          = undef,
   $max_files      = 1024,
   $max_processes  = 4096,
 ){
@@ -34,12 +39,20 @@ class nova::compute::libvirt::qemu(
 
   if $configure_qemu {
 
+    $augues_changes_default = [
+      "set max_files ${max_files}",
+      "set max_processes ${max_processes}",
+    ]
+    if $group and !empty($group) {
+      $augues_group_changes = ["set group ${group}"]
+    } else {
+      $augues_group_changes = []
+    }
+    $augues_changes = concat($augues_changes_default, $augues_group_changes)
+
     augeas { 'qemu-conf-limits':
       context => '/files/etc/libvirt/qemu.conf',
-      changes => [
-        "set max_files ${max_files}",
-        "set max_processes ${max_processes}",
-      ],
+      changes => $augues_changes,
       tag     => 'qemu-conf-augeas',
     }
   } else {
@@ -48,6 +61,7 @@ class nova::compute::libvirt::qemu(
       changes => [
         'rm max_files',
         'rm max_processes',
+        'rm group',
       ],
       tag     => 'qemu-conf-augeas',
     }
