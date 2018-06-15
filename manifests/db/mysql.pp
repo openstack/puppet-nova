18e595f9461f1da4328093b9961db997efe48658
zhubingbing <zhu.bingbing@99cloud.net>
2018-06-15 15:07:37 +0800

    Fix annotations
    Change-Id: I78385b0ff748b519243009c34f88e29a2b2b2b03

--- manifests/db/mysql.pp
+++ manifests/db/mysql.pp
@@ -5,34 +5,34 @@
 # === Parameters:
 #
 # [*password*]
-#   Password to use for the nova user
+#   (Required) Password to use for the nova user
 #
 # [*dbname*]
-#   (optional) The name of the database
+#   (Optional) The name of the database
 #   Defaults to 'nova'
 #
 # [*user*]
-#   (optional) The mysql user to create
+#   (Optional) The mysql user to create
 #   Defaults to 'nova'
 #
 # [*host*]
-#   (optional) The IP address of the mysql server
+#   (Optional) The IP address of the mysql server
 #   Defaults to '127.0.0.1'
 #
 # [*charset*]
-#   (optional) The charset to use for the nova database
+#   (Optional) The charset to use for the nova database
 #   Defaults to 'utf8'
 #
 # [*collate*]
-#   (optional) The collate to use for the nova database
+#   (Optional) The collate to use for the nova database
 #   Defaults to 'utf8_general_ci'
 #
 # [*allowed_hosts*]
-#   (optional) Additional hosts that are allowed to access this DB
+#   (Optional) Additional hosts that are allowed to access this DB
 #   Defaults to undef
 #
 # [*setup_cell0*]
-#   (optional) Setup a cell0 for the cell_v2 functionality. This option will
+#   (Optional) Setup a cell0 for the cell_v2 functionality. This option will
 #   be set to true by default in Ocata when the cell v2 setup is mandatory.
 #   Defaults to false
 #
