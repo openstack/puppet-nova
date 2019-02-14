4042642e4941bf3dac5d305c876726a47a1ebf6f
ZhongShengping <chdzsp@163.com>
2019-02-14 17:23:07 +0800

    Service_token_roles_required missing in the server config file
    Service_token_roles_required missing in the server config file which
    allows backwards compatibility to ensure that the service tokens are
    compared against a list of possible roles for validity.
    
    Change-Id: I826a09ede0d15502da69cd0b48c5e54854e07f6c
    Closes-Bug: 1778198

--- spec/classes/nova_keystone_authtoken_spec.rb
+++ spec/classes/nova_keystone_authtoken_spec.rb
@@ -42,6 +42,7 @@ describe 'nova::keystone::authtoken' do
         is_expected.to contain_nova_config('keystone_authtoken/memcached_servers').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('keystone_authtoken/region_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('keystone_authtoken/token_cache_time').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_nova_config('keystone_authtoken/service_token_roles_required').with_value('<SERVICE DEFAULT>')
       end
     end
 
@@ -82,6 +83,7 @@ describe 'nova::keystone::authtoken' do
           :manage_memcache_package              => true,
           :region_name                          => 'region2',
           :token_cache_time                     => '301',
+          :service_token_roles_required         => false,
         })
       end
 
@@ -117,6 +119,7 @@ describe 'nova::keystone::authtoken' do
         is_expected.to contain_nova_config('keystone_authtoken/memcached_servers').with_value('memcached01:11211,memcached02:11211')
         is_expected.to contain_nova_config('keystone_authtoken/region_name').with_value(params[:region_name])
         is_expected.to contain_nova_config('keystone_authtoken/token_cache_time').with_value(params[:token_cache_time])
+        is_expected.to contain_nova_config('keystone_authtoken/service_token_roles_required').with_value(params[:service_token_roles_required])
       end
 
       it 'installs python memcache package' do
