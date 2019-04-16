openstack/puppet-nova

commit 958c5b2f35e0789b0e280ba1f7662ebe19e4bad6

Adds the cross_az_attach parameter
Adds the cross_az_attach parameter to allow configuring attach between
instance and volume in different availability zones.

Change-Id: I064f872e20537642880d5482cb9e0c2a801715ef
implements: blueprint split-controlplane-templates

manifests/init.pp
@@ -421,6 +421,10 @@
 #   (optional) Use IPv6 or not.
 #   Defaults to undef
 #
+#  [*cross_az_attach*]
+#   (optional) Allow attach between instance and volume in different availability zones.
+#   Defaults to $::os_service_default
+#
 class nova(
   $ensure_package                         = 'present',
   $database_connection                    = undef,
@@ -516,6 +520,7 @@ class nova(
   $image_service                          = undef,
   $notify_on_api_faults                   = undef,
   $use_ipv6                               = undef,
+  $cross_az_attach                        = $::os_service_default,
 ) inherits nova::params {
 
   include ::nova::deps
@@ -759,6 +764,7 @@ but should be one of: ssh-rsa, ssh-dsa, ssh-ecdsa.")
 
   nova_config {
     'cinder/os_region_name':      value => $os_region_name;
+    'cinder/cross_az_attach':     value => $cross_az_attach;
     'upgrade_levels/cells':       value => $upgrade_level_cells;
     'upgrade_levels/cert':        value => $upgrade_level_cert;
     'upgrade_levels/compute':     value => $upgrade_level_compute;

releasenotes/notes/add-cross-az-attach-parameters-3ebc84f8882dd941.yaml
@@ -1,0 +1,4 @@
+---
+features:
+  - Adds the cross_az_attach parameter to allow configuring attach between
+    instance and volume in different availability zones.

spec/classes/nova_init_spec.rb
@@ -53,6 +53,7 @@ describe 'nova' do
         is_expected.to contain_nova_config('DEFAULT/rpc_response_timeout').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('DEFAULT/control_exchange').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('cinder/os_region_name').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_nova_config('cinder/cross_az_attach').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('cinder/catalog_info').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('DEFAULT/cpu_allocation_ratio').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('DEFAULT/ram_allocation_ratio').with_value('<SERVICE DEFAULT>')
@@ -90,6 +91,7 @@ describe 'nova' do
           :notification_format                     => 'unversioned',
           :report_interval                         => '60',
           :os_region_name                          => 'MyRegion',
+          :cross_az_attach                         => 'MyAZ',
           :ovsdb_connection                        => 'tcp:127.0.0.1:6640',
           :use_ipv6                                => true,
           :upgrade_level_cells                     => '1.0.0',
@@ -172,6 +174,7 @@ describe 'nova' do
         is_expected.to contain_nova_config('DEFAULT/ovsdb_connection').with_value('tcp:127.0.0.1:6640')
         is_expected.to contain_nova_config('os_vif_linux_bridge/use_ipv6').with_value('true')
         is_expected.to contain_nova_config('cinder/os_region_name').with_value('MyRegion')
+        is_expected.to contain_nova_config('cinder/cross_az_attach').with_value('MyAZ')
         is_expected.to contain_nova_config('DEFAULT/ssl_only').with_value(true)
         is_expected.to contain_nova_config('DEFAULT/cert').with_value('/etc/ssl/private/snakeoil.pem')
         is_expected.to contain_nova_config('DEFAULT/key').with_value('/etc/ssl/certs/snakeoil.pem')

commit 366aaf227323b5af319a1617c5fe19558949cb4f

Move cross_az_attach above deprecated params
Commit 958c5b2f35e0789b0e280ba1f7662ebe19e4bad6 accidentally added the
new cross_az_attach parameter in the deprecated sections. This commit
just moves it up so it's clear this parameter is not deprecated.

Change-Id: Ib32dc7f21c916190f952c1ae5a353434a6969616

manifests/init.pp
@@ -394,6 +394,10 @@
 #   If unable to do so, will use "127.0.0.1".
 #   Defaults to $::os_service_default.
 #
+#  [*cross_az_attach*]
+#   (optional) Allow attach between instance and volume in different availability zones.
+#   Defaults to $::os_service_default
+#
 # DEPRECATED PARAMETERS
 #
 # [*placement_database_connection*]
@@ -420,10 +424,6 @@
 # [*use_ipv6*]
 #   (optional) Use IPv6 or not.
 #   Defaults to undef
-#
-#  [*cross_az_attach*]
-#   (optional) Allow attach between instance and volume in different availability zones.
-#   Defaults to $::os_service_default
 #
 class nova(
   $ensure_package                         = 'present',
@@ -513,6 +513,7 @@ class nova(
   $disk_allocation_ratio                  = $::os_service_default,
   $purge_config                           = false,
   $my_ip                                  = $::os_service_default,
+  $cross_az_attach                        = $::os_service_default,
   # DEPRECATED PARAMETERS
   $placement_database_connection          = undef,
   $placement_slave_connection             = undef,
@@ -520,7 +521,6 @@ class nova(
   $image_service                          = undef,
   $notify_on_api_faults                   = undef,
   $use_ipv6                               = undef,
-  $cross_az_attach                        = $::os_service_default,
 ) inherits nova::params {
 
   include ::nova::deps