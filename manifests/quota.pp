07993b785d500157f96d2215058809f749a7eb4b
Tobias Urdin <tobias.urdin@binero.se>
2019-05-10 17:17:39 +0200

    Remove deprecated quota parameters
    These were renamed to remove the quota_* prefix
    and the old ones and the pick()'s can now be removed.
    
    Change-Id: Ibe5b5f49386e101598afcab210944cdcf69c8eeb

--- manifests/quota.pp
+++ manifests/quota.pp
@@ -72,64 +72,6 @@
 #   (optional) Number of seconds between subsequent usage refreshes
 #   Defaults to 0
 #
-#### DEPRECATED PARAMS
-#
-# [*quota_instances*]
-#   (optional) Number of instances
-#   Defaults to undef
-#
-# [*quota_cores*]
-#   (optional) Number of cores
-#   Defaults to undef
-#
-# [*quota_ram*]
-#   (optional) Ram in MB
-#   Defaults to undef
-#
-# [*quota_floating_ips*]
-#   (optional) Number of floating IPs
-#   Defaults to undef
-#
-# [*quota_fixed_ips*]
-#   (optional) Number of fixed IPs (this should be at least the number of instances allowed)
-#   Defaults to undef
-#
-# [*quota_metadata_items*]
-#   (optional) Number of metadata items per instance
-#   Defaults to undef
-#
-# [*quota_injected_files*]
-#   (optional) Number of files that can be injected per instance
-#   Defaults to undef
-#
-# [*quota_injected_file_content_bytes*]
-#   (optional) Maximum size in bytes of injected files
-#   Defaults to undef
-#
-# [*quota_injected_file_path_length*]
-#   (optional) Maximum size in bytes of injected file path
-#   Defaults to undef
-#
-# [*quota_security_groups*]
-#   (optional) Number of security groups
-#   Defaults to undef
-#
-# [*quota_security_group_rules*]
-#   (optional) Number of security group rules
-#   Defaults to undef
-#
-# [*quota_key_pairs*]
-#   (optional) Number of key pairs
-#   Defaults to undef
-#
-# [*quota_server_groups*]
-#   (optional) Number of server groups per project
-#   Defaults to undef
-#
-# [*quota_server_group_members*]
-#   (optional) Number of servers per server group
-#   Defaults to undef
-#
 class nova::quota(
   $instances                         = 10,
   $cores                             = 20,
@@ -148,56 +90,25 @@ class nova::quota(
   $reservation_expire                = 86400,
   $until_refresh                     = 0,
   $max_age                           = 0,
-  # DEPRECATED PARAMS
-  $quota_instances                   = undef,
-  $quota_cores                       = undef,
-  $quota_ram                         = undef,
-  $quota_floating_ips                = undef,
-  $quota_fixed_ips                   = undef,
-  $quota_metadata_items              = undef,
-  $quota_injected_files              = undef,
-  $quota_injected_file_content_bytes = undef,
-  $quota_injected_file_path_length   = undef,
-  $quota_security_groups             = undef,
-  $quota_security_group_rules        = undef,
-  $quota_key_pairs                   = undef,
-  $quota_server_groups               = undef,
-  $quota_server_group_members        = undef,
 ) {
 
   include ::nova::deps
 
-  # TODO(tobias-urdin): Remove these params and picks in the T release.
-  $instances_real = pick($quota_instances, $instances)
-  $cores_real = pick($quota_cores, $cores)
-  $ram_real = pick($quota_ram, $ram)
-  $floating_ips_real = pick($quota_floating_ips, $floating_ips)
-  $fixed_ips_real = pick($quota_fixed_ips, $fixed_ips)
-  $metadata_items_real = pick($quota_metadata_items, $metadata_items)
-  $injected_files_real = pick($quota_injected_files, $injected_files)
-  $injected_file_content_bytes_real = pick($quota_injected_file_content_bytes, $injected_file_content_bytes)
-  $injected_file_path_length_real = pick($quota_injected_file_path_length, $injected_file_path_length)
-  $security_groups_real = pick($quota_security_groups, $security_groups)
-  $security_group_rules_real = pick($quota_security_group_rules, $security_group_rules)
-  $key_pairs_real = pick($quota_key_pairs, $key_pairs)
-  $server_groups_real = pick($quota_server_groups, $server_groups)
-  $server_group_members_real = pick($quota_server_group_members, $server_group_members)
-
   nova_config {
-    'quota/instances':                   value => $instances_real;
-    'quota/cores':                       value => $cores_real;
-    'quota/ram':                         value => $ram_real;
-    'quota/floating_ips':                value => $floating_ips_real;
-    'quota/fixed_ips':                   value => $fixed_ips_real;
-    'quota/metadata_items':              value => $metadata_items_real;
-    'quota/injected_files':              value => $injected_files_real;
-    'quota/injected_file_content_bytes': value => $injected_file_content_bytes_real;
-    'quota/injected_file_path_length':   value => $injected_file_path_length_real;
-    'quota/security_groups':             value => $security_groups_real;
-    'quota/security_group_rules':        value => $security_group_rules_real;
-    'quota/key_pairs':                   value => $key_pairs_real;
-    'quota/server_groups':               value => $server_groups_real;
-    'quota/server_group_members':        value => $server_group_members_real;
+    'quota/instances':                   value => $instances;
+    'quota/cores':                       value => $cores;
+    'quota/ram':                         value => $ram;
+    'quota/floating_ips':                value => $floating_ips;
+    'quota/fixed_ips':                   value => $fixed_ips;
+    'quota/metadata_items':              value => $metadata_items;
+    'quota/injected_files':              value => $injected_files;
+    'quota/injected_file_content_bytes': value => $injected_file_content_bytes;
+    'quota/injected_file_path_length':   value => $injected_file_path_length;
+    'quota/security_groups':             value => $security_groups;
+    'quota/security_group_rules':        value => $security_group_rules;
+    'quota/key_pairs':                   value => $key_pairs;
+    'quota/server_groups':               value => $server_groups;
+    'quota/server_group_members':        value => $server_group_members;
     'quota/reservation_expire':          value => $reservation_expire;
     'quota/until_refresh':               value => $until_refresh;
     'quota/max_age':                     value => $max_age;
--- spec/classes/nova_quota_spec.rb
+++ spec/classes/nova_quota_spec.rb
@@ -69,44 +69,6 @@ describe 'nova::quota' do
 
       it_behaves_like 'nova::quota config options'
     end
-
-    context 'with deprecated parameters' do
-      before do
-        params.merge!({
-          :quota_instances                   => 20,
-          :quota_cores                       => 40,
-          :quota_ram                         => 102400,
-          :quota_floating_ips                => 20,
-          :quota_fixed_ips                   => 512,
-          :quota_metadata_items              => 256,
-          :quota_injected_files              => 10,
-          :quota_injected_file_content_bytes => 20480,
-          :quota_injected_file_path_length   => 254,
-          :quota_security_groups             => 20,
-          :quota_security_group_rules        => 40,
-          :quota_key_pairs                   => 200,
-          :quota_server_groups               => 20,
-          :quota_server_group_members        => 20
-        })
-      end
-
-      it {
-        should contain_nova_config('quota/instances').with_value(params[:quota_instances])
-        should contain_nova_config('quota/cores').with_value(params[:quota_cores])
-        should contain_nova_config('quota/ram').with_value(params[:quota_ram])
-        should contain_nova_config('quota/floating_ips').with_value(params[:quota_floating_ips])
-        should contain_nova_config('quota/fixed_ips').with_value(params[:quota_fixed_ips])
-        should contain_nova_config('quota/metadata_items').with_value(params[:quota_metadata_items])
-        should contain_nova_config('quota/injected_files').with_value(params[:quota_injected_files])
-        should contain_nova_config('quota/injected_file_content_bytes').with_value(params[:quota_injected_file_content_bytes])
-        should contain_nova_config('quota/injected_file_path_length').with_value(params[:quota_injected_file_path_length])
-        should contain_nova_config('quota/security_groups').with_value(params[:quota_security_groups])
-        should contain_nova_config('quota/security_group_rules').with_value(params[:quota_security_group_rules])
-        should contain_nova_config('quota/key_pairs').with_value(params[:quota_key_pairs])
-        should contain_nova_config('quota/server_groups').with_value(params[:quota_server_groups])
-        should contain_nova_config('quota/server_group_members').with_value(params[:quota_server_group_members])
-      }
-    end
   end
 
   on_supported_os({
