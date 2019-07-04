7c1717af69d2659703833e9fefe9af86664d2e29
Martin Schuppert <mschuppert@redhat.com>
2019-07-04 08:56:51 +0200

    Deprecate oslo::middleware related parameters in ::nova::metadata
    enable_proxy_headers_parsing should not only be configurabe if
    !$nova_metadata_wsgi_enabled , Therefore it needs to be moved
    outside the condition.
    But at the moment enable_proxy_headers_parsing can also be set via
    ::nova::metadata which conficts.
    This change deprecates oslo::middleware related parameters
    enable_proxy_headers_parsing and and max_request_body_size in
    ::nova::metadata. In future ::nova::api should be used instead.
    
    Change-Id: I0f138bf278931ac21d228251cc1da44638da915c

--- spec/classes/nova_metadata_spec.rb
+++ spec/classes/nova_metadata_spec.rb
@@ -21,11 +21,6 @@ describe 'nova::metadata' do
 
       it 'configures various stuff' do
         is_expected.to contain_nova_config('api/metadata_cache_expiration').with('value' => '<SERVICE DEFAULT>')
-        is_expected.to contain_oslo__middleware('nova_config').with(
-          :enable_proxy_headers_parsing => '<SERVICE DEFAULT>',
-          :max_request_body_size        => '<SERVICE DEFAULT>',
-        )
-        is_expected.to contain_nova_config('api/metadata_cache_expiration').with('value' => '<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('api/local_metadata_per_cell').with('value' => '<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('api/dhcp_domain').with('value' => '<SERVICE DEFAULT>')
       end
@@ -39,12 +34,10 @@ describe 'nova::metadata' do
     context 'with overridden parameters' do
       before do
         params.merge!({
-          :neutron_metadata_proxy_shared_secret => 'secrete',
-          :enable_proxy_headers_parsing         => true,
-          :max_request_body_size                => '102400',
-          :local_metadata_per_cell              => true,
-          :metadata_cache_expiration            => 15,
-          :dhcp_domain                          => 'foo',
+          :neutron_metadata_proxy_shared_secret        => 'secrete',
+          :local_metadata_per_cell                     => true,
+          :metadata_cache_expiration                   => 15,
+          :dhcp_domain                                 => 'foo',
         })
       end
 
@@ -54,10 +47,6 @@ describe 'nova::metadata' do
         is_expected.to contain_nova_config('api/dhcp_domain').with('value' => 'foo')
         is_expected.to contain_nova_config('neutron/service_metadata_proxy').with('value' => true)
         is_expected.to contain_nova_config('neutron/metadata_proxy_shared_secret').with('value' => 'secrete').with_secret(true)
-        is_expected.to contain_oslo__middleware('nova_config').with(
-          :enable_proxy_headers_parsing => true,
-          :max_request_body_size        => '102400',
-        )
       end
     end
 
