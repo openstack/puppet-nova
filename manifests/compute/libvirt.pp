4120d79e132e853a9ee5ace9f78319bc3ee450c4
Martin Schuppert <mschuppert@redhat.com>
2019-08-16 14:56:45 +0200

    Adds tls_priority parameter to nova::compute::libvirt
    Override the compile time default TLS priority string. The
    default is usually "NORMAL" unless overridden at build time.
    Only set this if it is desired for libvirt to deviate from
    the global default settings.
    
    Conflicts:
          manifests/compute/libvirt.pp
    
    Change-Id: I221d48ba720e8ad820050fb5f735cd20d75f2f7a
    Related-Bug: #1840447
    (cherry picked from commit 7f9c58f86cb4c7505fa04f0ad7e723ada09034ba)
    (cherry picked from commit b28b01a01f940af2356d348cf66d2dcdfa8ef25a)
    (cherry picked from commit ef1499a4c6e7fe986bbf84158a7abfc2169613fc)

--- manifests/compute/libvirt.pp
+++ manifests/compute/libvirt.pp
@@ -159,6 +159,13 @@
 #   https://libvirt.org/logging.html
 #   Defaults to undef
 #
+# [*tls_priority*]
+#   (optional) Override the compile time default TLS priority string. The
+#   default is usually "NORMAL" unless overridden at build time.
+#   Only set this if it is desired for libvirt to deviate from
+#   the global default settings.
+#   Defaults to undef
+#
 class nova::compute::libvirt (
   $ensure_package                             = 'present',
   $libvirt_virt_type                          = 'kvm',
@@ -189,6 +196,7 @@ class nova::compute::libvirt (
   $nfs_mount_options                          = $::os_service_default,
   $mem_stats_period_seconds                   = $::os_service_default,
   $log_filters                                = undef,
+  $tls_priority                               = undef,
 ) inherits nova::params {
 
   include ::nova::deps
@@ -228,6 +236,12 @@ class nova::compute::libvirt (
   if $log_filters {
     libvirtd_config {
       'log_filters': value => "\"${log_filters}\"";
+    }
+  }
+
+  if $tls_priority {
+    libvirtd_config {
+      'tls_priority': value => "\"${tls_priority}\"";
     }
   }
 
