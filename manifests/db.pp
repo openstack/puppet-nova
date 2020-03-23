abc57251bd21382dfc52d8c8741dd0f35fc2f0a1
ZhongShengping <chdzsp@163.com>
2020-03-23 14:45:49 +0800

    Remove idle_timeout option
    The idle_timeout parameter has been deprecated for two releases.
    We can remove it.
    
    Change-Id: Ic248ff1250255ed8a9b8c9aecb077cbb67602d29

--- manifests/db.pp
+++ manifests/db.pp
@@ -71,12 +71,6 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
-# DEPRECATED PARAMETERS
-#
-# [*database_idle_timeout*]
-#   Timeout when db connections should be reaped.
-#   Defaults to undef.
-#
 class nova::db (
   $database_db_max_retries          = $::os_service_default,
   $database_connection              = $::os_service_default,
@@ -90,17 +84,10 @@ class nova::db (
   $database_retry_interval          = $::os_service_default,
   $database_max_overflow            = $::os_service_default,
   $database_pool_timeout            = $::os_service_default,
-  # DEPRECATED PARAMETERS
-  $database_idle_timeout            = undef,
 ) {
 
   include nova::deps
   include nova::params
-
-  if $database_idle_timeout {
-    warning('The database_idle_timeout parameter is deprecated. Please use \
-database_connection_recycle_time instead.')
-  }
 
   # NOTE(spredzy): In order to keep backward compatibility we rely on the pick function
   # to use nova::<myparam> first the nova::db::<myparam>
@@ -108,7 +95,7 @@ database_connection_recycle_time instead.')
   $slave_connection_real = pick($::nova::slave_connection, $slave_connection)
   $api_database_connection_real = pick($::nova::api_database_connection, $api_database_connection)
   $api_slave_connection_real = pick($::nova::api_slave_connection, $api_slave_connection)
-  $database_connection_recycle_time_real = pick($::nova::database_idle_timeout, $database_idle_timeout, $database_connection_recycle_time)
+  $database_connection_recycle_time_real = pick($::nova::database_idle_timeout, $database_connection_recycle_time)
   $database_min_pool_size_real = pick($::nova::database_min_pool_size, $database_min_pool_size)
   $database_max_pool_size_real = pick($::nova::database_max_pool_size, $database_max_pool_size)
   $database_max_retries_real = pick($::nova::database_max_retries, $database_max_retries)
