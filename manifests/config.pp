b5c5d7acc3cc2cf2836a4f4556543d3fe1737b95
Takashi Kajinami <tkajinam@redhat.com>
2020-05-02 23:50:29 +0900

    Rename nova_paste_api_ini to nova_api_paste_ini
    ... so that the name is consistent with the other puppet modules.
    
    Change-Id: I8237b7760f3f7a7bf2806524b2582ef85d3bc6d5

--- manifests/config.pp
+++ manifests/config.pp
@@ -20,6 +20,15 @@
 #   NOTE: The configuration MUST NOT be already handled by this module
 #   or Puppet catalog compilation will fail with duplicate resources.
 #
+# [*nova_api_paste_ini*]
+#   (optional) Allow configuration of arbitrary Nova paste api configurations.
+#   The value is an hash of nova_paste_api_ini resources. Example:
+#   { 'DEFAULT/foo' => { value => 'fooValue'},
+#     'DEFAULT/bar' => { value => 'barValue'}
+#   }
+#
+# DEPRECATED PARAMETERS
+#
 # [*nova_paste_api_ini*]
 #   (optional) Allow configuration of arbitrary Nova paste api configurations.
 #   The value is an hash of nova_paste_api_ini resources. Example:
@@ -29,14 +38,24 @@
 #
 class nova::config (
   $nova_config        = {},
-  $nova_paste_api_ini = {},
+  $nova_api_paste_ini = {},
+  # DEPRECATED PARAMETERS
+  $nova_paste_api_ini = undef,
 ) {
 
   include nova::deps
 
+  if $nova_paste_api_ini != undef {
+    warning('nova_paste_api_ini is deprecated and will be removed in a future
+release. Use nova_api_paste_init')
+    $nova_api_paste_ini_real = $nova_paste_api_ini
+  } else {
+    $nova_api_paste_ini_real = $nova_api_paste_ini
+  }
+
   validate_legacy(Hash, 'validate_hash', $nova_config)
-  validate_legacy(Hash, 'validate_hash', $nova_paste_api_ini)
+  validate_legacy(Hash, 'validate_hash', $nova_api_paste_ini_real)
 
   create_resources('nova_config', $nova_config)
-  create_resources('nova_paste_api_ini', $nova_paste_api_ini)
+  create_resources('nova_api_paste_ini', $nova_api_paste_ini_real)
 }
