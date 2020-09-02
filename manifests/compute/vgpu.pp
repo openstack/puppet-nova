d9cd5ad37d48910ffbc8dd8e21e48911bb2085f4
Rajesh Tailor <ratailor@redhat.com>
2020-09-02 18:15:28 +0530

    Add new parameter `vgpu_types_device_addresses_mapping`
    To support multiple vgpu types configuration, add new
    parameter `vgpu_types_device_addresses_mapping` where vgpu-type
    is key and list of device_addresses are value.
    
    Also deprecate `enabled_vgpu_types` parameter and instead
    use newly added parameter.
    
    Change-Id: I2e66a68ad831e2d25a757793babaf31277c11eb2
    (cherry picked from commit fbf77c0776f2aac341dc7a54a6a82243681d6354)

--- manifests/compute/vgpu.pp
+++ manifests/compute/vgpu.pp
@@ -4,17 +4,55 @@
 #
 # === Parameters:
 #
-#  [*enabled_vgpu_types*]
+# [*vgpu_types_device_addresses_mapping*]
+#   (optional) Map of vgpu type(s) the instances can get as key and list of
+#   corresponding device addresses as value.
+#   Defaults to {}
+#
+#  DEPRECATED PARAMETERS
+#
+# [*enabled_vgpu_types*]
 #   (optional) Specify which specific GPU type(s) the instances can get
-#   Defaults to $::os_service_default
-#   Example: 'nvidia-35' or ['nvidia-35', 'nvidia-36']
+#   Defaults to undef
+#
 
 class nova::compute::vgpu(
-  $enabled_vgpu_types = $::os_service_default
+  $vgpu_types_device_addresses_mapping      = {},
+  # DEPRECATED PARAMETERS
+  $enabled_vgpu_types                       = undef,
 ) {
   include nova::deps
 
-  nova_config {
-    'devices/enabled_vgpu_types': value => join(any2array($enabled_vgpu_types), ',');
+  if $enabled_vgpu_types {
+    warning('enabled_vgpu_types is deprecated, instead use vgpu_types_device_addresses_mapping parameter.')
+
+    if !empty($vgpu_types_device_addresses_mapping) {
+      warning('vgpu_types_device_addresses_mapping is ignored, when both enabled_vgpu_types \
+and vgpu_types_device_addresses_mapping are defined.')
+    }
+  }
+
+  if $enabled_vgpu_types != undef and !empty($enabled_vgpu_types) {
+    nova_config {
+      'devices/enabled_vgpu_types': value => join(any2array($enabled_vgpu_types), ',');
+    }
+  } elsif !empty($vgpu_types_device_addresses_mapping) {
+    validate_legacy(Hash, 'validate_hash', $vgpu_types_device_addresses_mapping)
+    $vgpu_types_real = keys($vgpu_types_device_addresses_mapping)
+    nova_config {
+      'devices/enabled_vgpu_types': value => join(any2array($vgpu_types_real), ',');
+    }
+
+    $vgpu_types_device_addresses_mapping.each |$vgpu_type, $device_addresses| {
+      if !empty($device_addresses) {
+        nova_config {
+          "vgpu_${vgpu_type}/device_addresses": value => join(any2array($device_addresses), ',');
+        }
+      }
+    }
+  } else {
+    nova_config {
+      'devices/enabled_vgpu_types': ensure => absent;
+    }
   }
 }
