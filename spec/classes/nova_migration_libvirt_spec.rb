9f8c4c9785cb2f7ddcb5ac1fa5cd9408bb0593cf
Rocky <shi.yan@unimelb.edu.au>
2020-10-14 21:39:15 +1100

    Add support for more migration parameters
    Change-Id: I4a6c05e47a0e1ce2e6498a2ccbf075e0f1216335

--- spec/classes/nova_migration_libvirt_spec.rb
+++ spec/classes/nova_migration_libvirt_spec.rb
@@ -42,6 +42,8 @@ describe 'nova::migration::libvirt' do
       it { is_expected.to contain_nova_config('libvirt/live_migration_completion_timeout').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_nova_config('libvirt/live_migration_uri').with_value('qemu+tcp://%s/system') }
       it { is_expected.to contain_nova_config('libvirt/live_migration_inbound_addr').with_value('<SERVICE DEFAULT>')}
+      it { is_expected.to contain_nova_config('libvirt/live_migration_permit_post_copy').with_value('<SERVICE DEFAULT>')}
+      it { is_expected.to contain_nova_config('libvirt/live_migration_permit_auto_converge').with_value('<SERVICE DEFAULT>')}
     end
 
     context 'with override_uuid enabled' do
@@ -107,6 +109,17 @@ describe 'nova::migration::libvirt' do
       end
       it { is_expected.to contain_nova_config('libvirt/live_migration_tunnelled').with(:value => true) }
       it { is_expected.to contain_nova_config('libvirt/live_migration_completion_timeout').with_value('1500') }
+    end
+
+    context 'with live migration auto converge on' do
+      let :params do
+        {
+          :live_migration_permit_post_copy     => false,
+          :live_migration_permit_auto_converge => true,
+        }
+      end
+      it { is_expected.to contain_nova_config('libvirt/live_migration_permit_post_copy').with(:value => false) }
+      it { is_expected.to contain_nova_config('libvirt/live_migration_permit_auto_converge').with(:value => true) }
     end
 
     context 'with auth set to sasl' do
