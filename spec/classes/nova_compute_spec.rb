8a59d08d5e9a501b525a6219fc173a7590660db9
Rocky <shi.yan@unimelb.edu.au>
2020-10-29 11:39:41 +1100

    Add default_ephemeral_format config option
    Change-Id: Id8b024f2ff5002a564d1719a3ca3251fe5d1ccf0
    (cherry picked from commit 1c19eb0a87a69289e72c8d0ec778aa766a3139a9)

--- spec/classes/nova_compute_spec.rb
+++ spec/classes/nova_compute_spec.rb
@@ -68,6 +68,7 @@ describe 'nova::compute' do
       it { is_expected.to contain_nova_config('DEFAULT/running_deleted_instance_poll_interval').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_nova_config('DEFAULT/running_deleted_instance_timeout').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_nova_config('DEFAULT/compute_monitors').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_nova_config('DEFAULT/default_ephemeral_format').with_value('<SERVICE DEFAULT>') }
 
       it { is_expected.to_not contain_package('bridge-utils').with(
         :ensure => 'present',
@@ -120,6 +121,7 @@ describe 'nova::compute' do
           :running_deleted_instance_poll_interval => '900',
           :running_deleted_instance_timeout   => '200',
           :compute_monitors                   => ['cpu.virt_driver','fake'],
+          :default_ephemeral_format           => 'ext4',
         }
       end
 
@@ -183,6 +185,7 @@ describe 'nova::compute' do
       it { is_expected.to contain_nova_config('DEFAULT/running_deleted_instance_poll_interval').with_value('900') }
       it { is_expected.to contain_nova_config('DEFAULT/running_deleted_instance_timeout').with_value('200') }
       it { is_expected.to contain_nova_config('DEFAULT/compute_monitors').with_value('cpu.virt_driver,fake') }
+      it { is_expected.to contain_nova_config('DEFAULT/default_ephemeral_format').with_value('ext4') }
 
       it 'configures nova config_drive_format to vfat' do
         is_expected.to contain_nova_config('DEFAULT/config_drive_format').with_value('vfat')
