9f2e3882d5ff23c8a33633d35e3b93ffebbc7a8e
ZhongShengping <chdzsp@163.com>
2020-11-02 14:42:04 +0800

    Deprecate allow_insecure_clients option
    The allow_insecure_clients has been deprecated[1].
    
    [1]https://review.opendev.org/#/c/417629/
    
    Change-Id: I1130125fb877146ea8b0c531f1ef5854aa211c33
    Closes-Bug: #1902158

--- spec/classes/nova_init_spec.rb
+++ spec/classes/nova_init_spec.rb
@@ -307,7 +307,6 @@ describe 'nova' do
         is_expected.to contain_nova_config('oslo_messaging_amqp/ssl_cert_file').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('oslo_messaging_amqp/ssl_key_file').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('oslo_messaging_amqp/ssl_key_password').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_nova_config('oslo_messaging_amqp/allow_insecure_clients').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('oslo_messaging_amqp/sasl_mechanisms').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('oslo_messaging_amqp/sasl_config_dir').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_nova_config('oslo_messaging_amqp/sasl_config_name').with_value('<SERVICE DEFAULT>')
