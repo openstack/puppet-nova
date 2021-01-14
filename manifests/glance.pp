896bf1ff5e17543197d6fb759bf293879481c909
Takashi Kajinami <tkajinam@redhat.com>
2021-01-14 09:48:36 +0900

    Add support for image download via rbd
    This change introduces some new parameters to the nova::glance class,
    which are required for the new feature to download glance image via
    RBD instead of API[1].
    
    [1] https://review.opendev.org/c/openstack/nova/+/574301
    
    Change-Id: I0c9ebc5fd980204c59cc903372820e1ee1245630

--- manifests/glance.pp
+++ manifests/glance.pp
@@ -12,9 +12,37 @@
 #   (optional) Number of retries in glance operation
 #   Defaults to $::os_service_default
 #
+# [*enable_rbd_download*]
+#   (optional) Enable download of Glance images directly via RBD
+#   Defaults to $::os_service_default
+#
+# [*rbd_user*]
+#   (optional) The RADOS client name for accessing Glance images stored as
+#   rbd volumes.
+#   Defaults to $::os_service_default
+#
+# [*rbd_connect_timeout*]
+#   (optional) THe RADOS client timeout in seconds when initially connecting
+#   to the cluster.
+#   Defaults to $::os_service_default
+#
+# [*rbd_pool*]
+#   (optional) The RADOS pool in which the Glance images are stored as rbd
+#   volumes.
+#   Defaults to $::os_service_default
+#
+# [*rbd_ceph_conf*]
+#   (optional) Path to the ceph configuration file to use.
+#   Defaults to $::os_service_default
+#
 class nova::glance (
-  $endpoint_override = $::os_service_default,
-  $num_retries       = $::os_service_default,
+  $endpoint_override   = $::os_service_default,
+  $num_retries         = $::os_service_default,
+  $enable_rbd_download = $::os_service_default,
+  $rbd_user            = $::os_service_default,
+  $rbd_connect_timeout = $::os_service_default,
+  $rbd_pool            = $::os_service_default,
+  $rbd_ceph_conf       = $::os_service_default,
 ) {
 
   include nova::deps
@@ -23,7 +51,12 @@ class nova::glance (
   $num_retries_real = pick($::nova::glance_num_retries, $num_retries)
 
   nova_config {
-    'glance/endpoint_override': value => $endpoint_override_real;
-    'glance/num_retries':       value => $num_retries_real;
+    'glance/endpoint_override':   value => $endpoint_override_real;
+    'glance/num_retries':         value => $num_retries_real;
+    'glance/enable_rbd_download': value => $enable_rbd_download;
+    'glance/rbd_user':            value => $rbd_user;
+    'glance/rbd_connect_timeout': value => $rbd_connect_timeout;
+    'glance/rbd_pool':            value => $rbd_pool;
+    'glance/rbd_ceph_conf':       value => $rbd_ceph_conf;
   }
 }
