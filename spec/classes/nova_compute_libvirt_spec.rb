cc03ec417660e49a0307c543d574de6d4a16784e
Martin Schuppert <mschuppert@redhat.com>
2021-01-18 15:36:36 +0100

    Add parameter to configure the libvirt max_queues
    The maximum number of virtio queue pairs that can be enabled when
    creating a multiqueue guest. The number of virtio queues allocated
    will be the lesser of the CPUs requested by the guest and the max
    value defined.
    
    Conflicts:
      manifests/compute/libvirt.pp
      spec/classes/nova_compute_libvirt_spec.rb
    
    Change-Id: Ieaa29b51257f5ea3a5e4d6c678140fd9ae052d88
    (cherry picked from commit 8a6de56193a18678927b4584917246df6bb89b63)
    (cherry picked from commit eed4d2dc1bf1db3913edd8dd2fa1140fb4a1abee)

--- spec/classes/nova_compute_libvirt_spec.rb
+++ spec/classes/nova_compute_libvirt_spec.rb
@@ -67,6 +67,7 @@ describe 'nova::compute::libvirt' do
       it { is_expected.to contain_nova_config('libvirt/num_pcie_ports').with_value('<SERVICE DEFAULT>')}
       it { is_expected.to contain_nova_config('libvirt/mem_stats_period_seconds').with_value('<SERVICE DEFAULT>')}
       it { is_expected.to contain_nova_config('libvirt/pmem_namespaces').with_value('<SERVICE DEFAULT>')}
+      it { is_expected.to contain_nova_config('libvirt/max_queues').with_value('<SERVICE DEFAULT>')}
       it { is_expected.to contain_libvirtd_config('log_outputs').with_ensure('absent')}
       it { is_expected.to contain_libvirtd_config('log_filters').with_ensure('absent')}
       it { is_expected.to contain_libvirtd_config('tls_priority').with_ensure('absent')}
@@ -102,7 +103,8 @@ describe 'nova::compute::libvirt' do
           :mem_stats_period_seconds                   => 20,
           :log_filters                                => '1:qemu',
           :tls_priority                               => 'NORMAL:-VERS-SSL3.0',
-          :pmem_namespaces                            => '128G:ns0|ns1|ns2|ns3'
+          :pmem_namespaces                            => '128G:ns0|ns1|ns2|ns3',
+          :max_queues                                 => 4,
         }
       end
 
@@ -135,6 +137,7 @@ describe 'nova::compute::libvirt' do
       it { is_expected.to contain_nova_config('libvirt/pmem_namespaces').with_value("128G:ns0|ns1|ns2|ns3")}
       it { is_expected.to contain_libvirtd_config('log_filters').with_value("\"#{params[:log_filters]}\"")}
       it { is_expected.to contain_libvirtd_config('tls_priority').with_value("\"#{params[:tls_priority]}\"")}
+      it { is_expected.to contain_nova_config('libvirt/max_queues').with_value(4)}
       it {
         is_expected.to contain_service('libvirt').with(
           :name     => 'custom_service',
