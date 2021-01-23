d18ad5bcc7358db9c71f1326f8b6a83784fdf725
Takashi Kajinami <tkajinam@redhat.com>
2021-01-24 08:41:34 +0900

    Clean up the deprecated neutron_* parameters
    This change removes the deprecated neutorn_* parameters from
    nova::network::neutron, which were deprecated during the previous
    Victoria cycle.
    
    Change-Id: I6c627855d6c90ae8ad18898fb8c2adb8d73477e1

--- spec/classes/nova_network_neutron_spec.rb
+++ spec/classes/nova_network_neutron_spec.rb
@@ -93,41 +93,6 @@ describe 'nova::network::neutron' do
         should contain_nova_config('DEFAULT/vif_plugging_timeout').with_value(params[:vif_plugging_timeout])
       end
     end
-
-    context 'when overriding deprecated class parameters' do
-      before do
-        params.merge!(
-          :password                        => false,
-          :neutron_password                => 's3cr3t',
-          :neutron_timeout                 => '30',
-          :neutron_project_name            => 'openstack',
-          :neutron_project_domain_name     => 'openstack_domain',
-          :neutron_region_name             => 'RegionTwo',
-          :neutron_username                => 'neutron2',
-          :neutron_user_domain_name        => 'neutron_domain',
-          :neutron_auth_url                => 'http://10.0.0.1:5000/v2',
-          :neutron_valid_interfaces        => 'public',
-          :neutron_endpoint_override       => 'http://127.0.0.1:9696',
-          :neutron_ovs_bridge              => 'br-int',
-          :neutron_extension_sync_interval => '600',
-        )
-      end
-
-      it 'configures neutron endpoint in nova.conf' do
-        should contain_nova_config('neutron/password').with_value(params[:neutron_password]).with_secret(true)
-        should contain_nova_config('neutron/timeout').with_value(params[:neutron_timeout])
-        should contain_nova_config('neutron/project_name').with_value(params[:neutron_project_name])
-        should contain_nova_config('neutron/project_domain_name').with_value(params[:neutron_project_domain_name])
-        should contain_nova_config('neutron/region_name').with_value(params[:neutron_region_name])
-        should contain_nova_config('neutron/username').with_value(params[:neutron_username])
-        should contain_nova_config('neutron/user_domain_name').with_value(params[:neutron_user_domain_name])
-        should contain_nova_config('neutron/auth_url').with_value(params[:neutron_auth_url])
-        should contain_nova_config('neutron/valid_interfaces').with_value(params[:neutron_valid_interfaces])
-        should contain_nova_config('neutron/endpoint_override').with_value(params[:neutron_endpoint_override])
-        should contain_nova_config('neutron/extension_sync_interval').with_value(params[:neutron_extension_sync_interval])
-        should contain_nova_config('neutron/ovs_bridge').with_value(params[:neutron_ovs_bridge])
-      end
-    end
   end
 
   on_supported_os({
