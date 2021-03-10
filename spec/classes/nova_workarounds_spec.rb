6178fbf0331b7bb1531365f578c56912d67fccb0
Christian Rohmann <christian.rohmann@inovex.de>
2021-03-10 16:46:54 +0100

    Expose [workaround]/ensure_libvirt_rbd_instance_dir_cleanup
    This exposes the nova workaround to remove the instance directory on
    clean up when using rbd.
    
    Change-Id: Iea47828c8022955cf0f91b891d43eff34a905174

--- spec/classes/nova_workarounds_spec.rb
+++ spec/classes/nova_workarounds_spec.rb
@@ -11,18 +11,21 @@ describe 'nova::workarounds' do
     context 'with default parameters' do
       it { is_expected.not_to contain_nova_config('workarounds/enable_numa_live_migration') }
       it { is_expected.to contain_nova_config('workarounds/never_download_image_if_on_rbd').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_nova_config('workarounds/ensure_libvirt_rbd_instance_dir_cleanup').with_value('<SERVICE DEFAULT>') }
     end
 
     context 'with overridden parameters' do
       let :params do
         {
           :enable_numa_live_migration => true,
-          :never_download_image_if_on_rbd => true
+          :never_download_image_if_on_rbd => true,
+          :ensure_libvirt_rbd_instance_dir_cleanup => true
         }
       end
 
       it { is_expected.to contain_nova_config('workarounds/enable_numa_live_migration').with_value('true') }
       it { is_expected.to contain_nova_config('workarounds/never_download_image_if_on_rbd').with_value('true') }
+      it { is_expected.to contain_nova_config('workarounds/ensure_libvirt_rbd_instance_dir_cleanup').with_value('true') }
     end
 
   end
