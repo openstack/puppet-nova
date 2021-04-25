d2aaab3683019a40bb8495f71c530657e198447b
Takashi Kajinami <tkajinam@redhat.com>
2021-04-25 22:17:49 +0900

    Centralize arbitrary configurations for libvirt daemons
    This is a prep work to add support for separate libvirt daemons and
    merge libvirt::config class and libvirtd::virtlogd::config class, so
    that we can put arbitary configurations to the single config class
    later.
    
    Change-Id: I21357822ae6113e484e94c6e6ab03976f40604eb

--- manifests/compute/libvirt/config.pp
+++ manifests/compute/libvirt/config.pp
@@ -17,14 +17,23 @@
 #     bar:
 #       value: barValue
 #
+# [*virtlogd_config*]
+#   (optional) Allow configuration of arbitrary virtlogd configurations.
+#   The value is an hash of virtlogd_config resources.
+#
 #   NOTE: The configuration MUST NOT be already handled by this module
 #   or Puppet catalog compilation will fail with duplicate resources.
 #
 class nova::compute::libvirt::config (
-  $libvirtd_config        = {},
+  $libvirtd_config = {},
+  $virtlogd_config = {},
 ) {
 
+  include nova::deps
+
   validate_legacy(Hash, 'validate_hash', $libvirtd_config)
+  validate_legacy(Hash, 'validate_hash', $virtlogd_config)
 
   create_resources('libvirtd_config', $libvirtd_config)
+  create_resources('virtlogd_config', $virtlogd_config)
 }
