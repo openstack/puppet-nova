ec99b385b924f3af9f469f5b95f07f48715844f1
Takashi Kajinami <tkajinam@redhat.com>
2021-06-30 16:34:15 +0900

    Victoria and before: Use libvirt-daemon-system in Ubuntu Focal
    ... because libvirt-bin is no longer available since Focal(20.04).
    This was fixed by e32576aebc31311e6dbc76b12e6a782481633fe8 during
    Wallaby cycle.
    
    Closes-Bug: #1933978
    Change-Id: I66f14ec424f9446bf8b5959e25abdbbd3213e51f

--- manifests/params.pp
+++ manifests/params.pp
@@ -71,10 +71,19 @@ class nova::params {
       $conductor_package_name            = 'nova-conductor'
       $novajoin_package_name             = undef
       $doc_package_name                  = 'nova-doc'
-      if ($::operatingsystem == 'Debian') and (versioncmp($::operatingsystemmajrelease, '9') >= 0 ) {
-        $libvirt_package_name            = 'libvirt-daemon-system'
+      if ($::operatingsystem == 'Debian') {
+        if (versioncmp($::operatingsystemmajrelease, '9') >= 0 ) {
+          $libvirt_package_name          = 'libvirt-daemon-system'
+        } else {
+          $libvirt_package_name          = 'libvirt-bin'
+        }
       } else {
-        $libvirt_package_name            = 'libvirt-bin'
+        # $::operatingsystem == 'Ubuntu'
+        if versioncmp($::operatingsystemmajrelease, '20.04') >= 0 {
+          $libvirt_package_name          = 'libvirt-daemon-system'
+        } else {
+          $libvirt_package_name          = 'libvirt-bin'
+        }
       }
       $scheduler_package_name            = 'nova-scheduler'
       $tgt_package_name                  = 'tgt'
