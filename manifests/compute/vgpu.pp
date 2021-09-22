d894523a6318276bf04e6ac952b55fcc7b6fb8c6
Takashi Kajinami <tkajinam@redhat.com>
2021-09-22 22:08:30 +0900

    Remove deprecated nova::compute::vgpu::enabled_vgpu_types
    ... because the parameter was deprecated during the Victoria cycle[1].
    
    [1] fbf77c0776f2aac341dc7a54a6a82243681d6354
    Change-Id: I13745fa1527fe6d4caa530033da4d6d5e0ed963f

--- manifests/compute/vgpu.pp
+++ manifests/compute/vgpu.pp
@@ -9,29 +9,12 @@
 #   corresponding device addresses as value.
 #   Defaults to {}
 #
-#  DEPRECATED PARAMETERS
-#
-# [*enabled_vgpu_types*]
-#   (optional) Specify which specific GPU type(s) the instances can get
-#   Defaults to undef
-#
-
 class nova::compute::vgpu(
-  $vgpu_types_device_addresses_mapping      = {},
-  # DEPRECATED PARAMETERS
-  $enabled_vgpu_types                       = undef,
+  $vgpu_types_device_addresses_mapping = {},
 ) {
   include nova::deps
 
-  if $enabled_vgpu_types {
-    warning('enabled_vgpu_types is deprecated, instead use vgpu_types_device_addresses_mapping parameter.')
-  }
-
-  if $enabled_vgpu_types != undef and !empty($enabled_vgpu_types) {
-    nova_config {
-      'devices/enabled_vgpu_types': value => join(any2array($enabled_vgpu_types), ',');
-    }
-  } elsif !empty($vgpu_types_device_addresses_mapping) {
+  if !empty($vgpu_types_device_addresses_mapping) {
     validate_legacy(Hash, 'validate_hash', $vgpu_types_device_addresses_mapping)
     $vgpu_types_real = keys($vgpu_types_device_addresses_mapping)
     nova_config {
