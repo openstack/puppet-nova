86b218072f6288aafd54f367d27f060b4a3d2d6c
Takashi Kajinami <tkajinam@redhat.com>
2022-01-05 22:10:41 +0900

    Remove usage of custom os_package_type fact
    Currently we support usage of distro packages only, and this custom
    fact can be simply replaced by the default fact.
    
    Change-Id: Ibc8f8bd63ad65d12ea790ec842e47f09f396bc08

--- manifests/wsgi/uwsgi_api_metadata.pp
+++ manifests/wsgi/uwsgi_api_metadata.pp
@@ -29,7 +29,7 @@ class nova::wsgi::uwsgi_api_metadata (
 
   include nova::deps
 
-  if $::os_package_type != 'debian'{
+  if $::operatingsystem != 'Debian'{
     warning('This class is only valid for Debian, as other operating systems are not using uwsgi by default.')
   }
 
