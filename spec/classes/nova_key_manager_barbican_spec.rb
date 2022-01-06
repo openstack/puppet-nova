956c8e5a93462f6415a29be29ccbed9782830a88
Takashi Kajinami <tkajinam@redhat.com>
2022-01-06 13:37:57 +0900

    key_manager: Add service user options for Barbican key manager
    This change introduces some parameters to set up the service user token
    feature for Barbican key manager, which was implemented during the Xena
    cycle[1].
    
    [1] 162039467ad0dfc5e25a16b75d9072d607690702
    
    Depends-on: https://review.opendev.org/810451
    Change-Id: I217e20e3d10fecffd3fb8654f047a7e285331723
    (cherry picked from commit 3453611d08ca802e6d18fefab450aee0e427fda9)

--- spec/classes/nova_key_manager_barbican_spec.rb
+++ spec/classes/nova_key_manager_barbican_spec.rb
@@ -5,13 +5,14 @@ describe 'nova::key_manager::barbican' do
     context 'with default parameters' do
       it {
         is_expected.to contain_oslo__key_manager__barbican('nova_config').with(
-          :barbican_endpoint      => '<SERVICE DEFAULT>',
-          :barbican_api_version   => '<SERVICE DEFAULT>',
-          :auth_endpoint          => '<SERVICE DEFAULT>',
-          :retry_delay            => '<SERVICE DEFAULT>',
-          :number_of_retries      => '<SERVICE DEFAULT>',
-          :barbican_endpoint_type => '<SERVICE DEFAULT>',
-          :barbican_region_name   => '<SERVICE DEFAULT>',
+          :barbican_endpoint       => '<SERVICE DEFAULT>',
+          :barbican_api_version    => '<SERVICE DEFAULT>',
+          :auth_endpoint           => '<SERVICE DEFAULT>',
+          :retry_delay             => '<SERVICE DEFAULT>',
+          :number_of_retries       => '<SERVICE DEFAULT>',
+          :barbican_endpoint_type  => '<SERVICE DEFAULT>',
+          :barbican_region_name    => '<SERVICE DEFAULT>',
+          :send_service_user_token => '<SERVICE DEFAULT>',
         )
 
         is_expected.to contain_package('cryptsetup').with(
@@ -24,25 +25,27 @@ describe 'nova::key_manager::barbican' do
     context 'with specified parameters' do
       let :params do
         {
-          :barbican_endpoint      => 'http://localhost:9311/',
-          :barbican_api_version   => 'v1',
-          :auth_endpoint          => 'http://localhost:5000',
-          :retry_delay            => 1,
-          :number_of_retries      => 60,
-          :barbican_endpoint_type => 'public',
-          :barbican_region_name   => 'regionOne',
+          :barbican_endpoint       => 'http://localhost:9311/',
+          :barbican_api_version    => 'v1',
+          :auth_endpoint           => 'http://localhost:5000',
+          :retry_delay             => 1,
+          :number_of_retries       => 60,
+          :barbican_endpoint_type  => 'public',
+          :barbican_region_name    => 'regionOne',
+          :send_service_user_token => true,
         }
       end
 
       it {
         is_expected.to contain_oslo__key_manager__barbican('nova_config').with(
-          :barbican_endpoint      => 'http://localhost:9311/',
-          :barbican_api_version   => 'v1',
-          :auth_endpoint          => 'http://localhost:5000',
-          :retry_delay            => 1,
-          :number_of_retries      => 60,
-          :barbican_endpoint_type => 'public',
-          :barbican_region_name   => 'regionOne',
+          :barbican_endpoint       => 'http://localhost:9311/',
+          :barbican_api_version    => 'v1',
+          :auth_endpoint           => 'http://localhost:5000',
+          :retry_delay             => 1,
+          :number_of_retries       => 60,
+          :barbican_endpoint_type  => 'public',
+          :barbican_region_name    => 'regionOne',
+          :send_service_user_token => true,
         )
 
         is_expected.to contain_package('cryptsetup').with(
