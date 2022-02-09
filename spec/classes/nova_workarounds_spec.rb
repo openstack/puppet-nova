35581554cb52821c4bc78b59d8f21ea379999148
Tobias Urdin <tobias.urdin@binero.se>
2022-02-09 08:22:03 +0000

    Add wait_for_vif_plugged_event_during_hard_reboot
    Adds parameter for the Nova workaround that waits
    for Neutron ML2 backend to sent vif plugged on
    hard reboot.
    
    Change-Id: I7fc60bf3ba35f270ab843df917d43e5deaf047fd
    (cherry picked from commit 84e565b1a04ce8d27a7621e88a0dbbc59fab982e)
    (cherry picked from commit a66c8b4c176ef16582f78903a436f540ec5ffff1)

--- spec/classes/nova_workarounds_spec.rb
+++ spec/classes/nova_workarounds_spec.rb
@@ -13,15 +13,17 @@ describe 'nova::workarounds' do
       it { is_expected.to contain_nova_config('workarounds/never_download_image_if_on_rbd').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_nova_config('workarounds/ensure_libvirt_rbd_instance_dir_cleanup').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_nova_config('workarounds/enable_qemu_monitor_announce_self').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_nova_config('workarounds/wait_for_vif_plugged_event_during_hard_reboot').with_value('<SERVICE DEFAULT>') }
     end
 
     context 'with overridden parameters' do
       let :params do
         {
-          :enable_numa_live_migration              => true,
-          :never_download_image_if_on_rbd          => true,
-          :ensure_libvirt_rbd_instance_dir_cleanup => true,
-          :enable_qemu_monitor_announce_self       => true,
+          :enable_numa_live_migration                    => true,
+          :never_download_image_if_on_rbd                => true,
+          :ensure_libvirt_rbd_instance_dir_cleanup       => true,
+          :enable_qemu_monitor_announce_self             => true,
+          :wait_for_vif_plugged_event_during_hard_reboot => ['normal', 'direct'],
         }
       end
 
@@ -29,6 +31,7 @@ describe 'nova::workarounds' do
       it { is_expected.to contain_nova_config('workarounds/never_download_image_if_on_rbd').with_value('true') }
       it { is_expected.to contain_nova_config('workarounds/ensure_libvirt_rbd_instance_dir_cleanup').with_value('true') }
       it { is_expected.to contain_nova_config('workarounds/enable_qemu_monitor_announce_self').with_value(true) }
+      it { is_expected.to contain_nova_config('workarounds/wait_for_vif_plugged_event_during_hard_reboot').with_value('normal,direct') }
     end
 
   end
