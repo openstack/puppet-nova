openstack/puppet-nova

commit 170edf57d8798d7b14770a73d93d0c00ee9bbd4a

Fix missing notifications about virt*d services
This change fixes the missing notifications between virt*d_config and
virt*d services, so that individual service is restarted when its
configuration file is updated.

This change also refactors how resource dependencies are managed, using
the existing anchors.

Closes-Bug: #1969401
Change-Id: Iff3ecdff9740dc1cc889b93666f089a8b60ef0ca

manifests/compute/libvirt/services.pp
@@ -102,11 +102,12 @@ class nova::compute::libvirt::services (
     }
 
     service { 'libvirt':
-      ensure  => $libvirt_service_ensure,
-      enable  => $libvirt_service_enable,
-      name    => $libvirt_service_name,
-      require => Anchor['nova::install::end'],
+      ensure => $libvirt_service_ensure,
+      enable => $libvirt_service_enable,
+      name   => $libvirt_service_name,
+      tag    => 'libvirt-service',
     }
+    Libvirtd_config<||> ~> Service['libvirt']
 
     # messagebus
     if($::osfamily == 'RedHat') {
@@ -114,8 +115,8 @@ class nova::compute::libvirt::services (
         ensure => running,
         enable => true,
         name   => $::nova::params::messagebus_service_name,
+        tag    => 'libvirt-service',
       }
-      Package['libvirt'] -> Service['messagebus']
     }
   }
 
@@ -124,8 +125,8 @@ class nova::compute::libvirt::services (
       ensure => running,
       enable => true,
       name   => $virtlock_service_name,
+      tag    => 'libvirt-service',
     }
-    Package<| title == 'libvirt' |> -> Service['virtlockd']
   }
 
   if $virtlog_service_name {
@@ -133,8 +134,9 @@ class nova::compute::libvirt::services (
       ensure => running,
       enable => true,
       name   => $virtlog_service_name,
+      tag    => 'libvirt-service',
     }
-    Package<| title == 'libvirt' |> -> Service['virtlogd']
+    Virtlogd_config<||> ~> Service['libvirt']
   }
 
   if ! $modular_libvirt {
@@ -157,43 +159,46 @@ class nova::compute::libvirt::services (
     if $virtsecret_service_name {
       package { 'virtsecret':
         ensure => present,
-        name   => "${::nova::params::libvirt_daemon_package_prefix}driver-secret"
+        name   => "${::nova::params::libvirt_daemon_package_prefix}driver-secret",
+        tag    => ['openstack', 'nova-support-package'],
       }
       service { 'virtsecretd':
-        ensure  => running,
-        enable  => true,
-        name    => $virtsecret_service_name,
-        require => Package['virtsecret'],
-        tag     => 'libvirt-modular-service',
+        ensure => running,
+        enable => true,
+        name   => $virtsecret_service_name,
+        tag    => ['libvirt-service', 'libvirt-modular-service'],
       }
+      Virtsecretd_config<||> ~> Service['virtlogd']
     }
 
     if $virtnodedev_service_name {
       package { 'virtnodedev':
         ensure => present,
-        name   => "${::nova::params::libvirt_daemon_package_prefix}driver-nodedev"
+        name   => "${::nova::params::libvirt_daemon_package_prefix}driver-nodedev",
+        tag    => ['openstack', 'nova-support-package'],
       }
       service { 'virtnodedevd':
-        ensure  => running,
-        enable  => true,
-        name    => $virtnodedev_service_name,
-        require => Package['virtnodedev'],
-        tag     => 'libvirt-modular-service',
+        ensure => running,
+        enable => true,
+        name   => $virtnodedev_service_name,
+        tag    => ['libvirt-service', 'libvirt-modular-service'],
       }
+      Virtnodedevd_config<||> ~> Service['virtlogd']
     }
 
     if $virtqemu_service_name {
       package { 'virtqemu':
         ensure => present,
-        name   => "${::nova::params::libvirt_daemon_package_prefix}driver-qemu"
+        name   => "${::nova::params::libvirt_daemon_package_prefix}driver-qemu",
+        tag    => ['openstack', 'nova-support-package'],
       }
       service { 'virtqemud':
-        ensure  => running,
-        enable  => true,
-        name    => $virtqemu_service_name,
-        require => Package['virtqemu'],
-        tag     => 'libvirt-modular-service',
+        ensure => running,
+        enable => true,
+        name   => $virtqemu_service_name,
+        tag    => ['libvirt-service', 'libvirt-modular-service'],
       }
+      Virtqemud_config<||> ~> Service['virtlogd']
     }
 
     if $virtproxy_service_name {
@@ -201,23 +206,24 @@ class nova::compute::libvirt::services (
         ensure => running,
         enable => true,
         name   => $virtproxy_service_name,
-        tag    => 'libvirt-modular-service',
+        tag    => ['libvirt-service', 'libvirt-modular-service'],
       }
-      Package<| title == 'libvirt' |> -> Service['virtproxyd']
+      Virtproxyd_config<||> ~> Service['virtlogd']
     }
 
     if $virtstorage_service_name {
       package { 'virtstorage':
         ensure => present,
-        name   => "${::nova::params::libvirt_daemon_package_prefix}driver-storage"
+        name   => "${::nova::params::libvirt_daemon_package_prefix}driver-storage",
+        tag    => ['openstack', 'nova-support-package'],
       }
       service { 'virtstoraged':
-        ensure  => running,
-        enable  => true,
-        name    => $virtstorage_service_name,
-        require => Package['virtstorage'],
-        tag     => 'libvirt-modular-service',
+        ensure => running,
+        enable => true,
+        name   => $virtstorage_service_name,
+        tag    => ['libvirt-service', 'libvirt-modular-service'],
       }
+      Virtstoraged_config<||> ~> Service['virtlogd']
     }
   }
 }

manifests/compute/libvirt/virtlogd.pp
@@ -72,8 +72,4 @@ class nova::compute::libvirt::virtlogd (
     'max_size':          value => pick($max_size, $::os_service_default);
     'max_backups':       value => pick($max_backups, $::os_service_default);
   }
-
-  Anchor['nova::config::begin']
-  -> Virtlogd_config<||>
-  -> Anchor['nova::config::end']
 }

manifests/compute/libvirt/virtnodedevd.pp
@@ -52,8 +52,4 @@ class nova::compute::libvirt::virtnodedevd (
     'admin_max_clients': value => $admin_max_clients;
     'ovs_timeout':       value => $ovs_timeout;
   }
-
-  Anchor['nova::config::begin']
-  -> Virtnodedevd_config<||>
-  -> Anchor['nova::config::end']
 }

manifests/compute/libvirt/virtproxyd.pp
@@ -61,8 +61,4 @@ class nova::compute::libvirt::virtproxyd (
     'ovs_timeout':       value => $ovs_timeout;
     'tls_priority':      value => $tls_priority, quote => true;
   }
-
-  Anchor['nova::config::begin']
-  -> Virtproxyd_config<||>
-  -> Anchor['nova::config::end']
 }

manifests/compute/libvirt/virtqemud.pp
@@ -53,8 +53,4 @@ class nova::compute::libvirt::virtqemud (
     'admin_max_clients': value => $admin_max_clients;
     'ovs_timeout':       value => $ovs_timeout;
   }
-
-  Anchor['nova::config::begin']
-  -> Virtqemud_config<||>
-  -> Anchor['nova::config::end']
 }

manifests/compute/libvirt/virtsecretd.pp
@@ -52,8 +52,4 @@ class nova::compute::libvirt::virtsecretd (
     'admin_max_clients': value => $admin_max_clients;
     'ovs_timeout':       value => $ovs_timeout;
   }
-
-  Anchor['nova::config::begin']
-  -> Virtsecretd_config<||>
-  -> Anchor['nova::config::end']
 }

manifests/compute/libvirt/virtstoraged.pp
@@ -38,9 +38,5 @@ class nova::compute::libvirt::virtstoraged (
     'log_outputs':       value => $log_outputs, quote => true;
     'ovs_timeout':       value => $ovs_timeout;
   }
-
-  Anchor['nova::config::begin']
-  -> Virtstoraged_config<||>
-  -> Anchor['nova::config::end']
 }
 

manifests/deps.pp
@@ -58,6 +58,20 @@ class nova::deps {
   -> Package<| tag == 'nova-support-package'|>
   -> Anchor['nova::install::end']
 
+  # Start libvirt services during the service phase
+  Anchor['nova::service::begin']
+  -> Service<| tag == 'libvirt-service'|>
+  -> Anchor['nova::service::end']
+
+  # Manage libvirt configurations during the config phase
+  Anchor['nova::config::begin'] -> Libvirtd_config<||> -> Anchor['nova::config::end']
+  Anchor['nova::config::begin'] -> Virtlogd_config<||> -> Anchor['nova::config::end']
+  Anchor['nova::config::begin'] -> Virtnodedevd_config<||> -> Anchor['nova::config::end']
+  Anchor['nova::config::begin'] -> Virtproxyd_config<||> -> Anchor['nova::config::end']
+  Anchor['nova::config::begin'] -> Virtqemud_config<||> -> Anchor['nova::config::end']
+  Anchor['nova::config::begin'] -> Virtsecretd_config<||> -> Anchor['nova::config::end']
+  Anchor['nova::config::begin'] -> Virtstoraged_config<||> -> Anchor['nova::config::end']
+
   # all cache settings should be applied and all packages should be installed
   # before service startup
   Oslo::Cache<||> -> Anchor['nova::service::begin']

commit 16ebf1bb8b2daedf449e1ed9259e7266709e9cb5

Fix wrong service notified by Virt*d_config
This change fixes the wrong notification which was introduced by
170edf57d8798d7b14770a73d93d0c00ee9bbd4a and ensures the correct
service is restarted.

Change-Id: Id8c38281fc8a69c1a83bd3806ac216556152759e

manifests/compute/libvirt/services.pp
@@ -136,7 +136,7 @@ class nova::compute::libvirt::services (
       name   => $virtlog_service_name,
       tag    => 'libvirt-service',
     }
-    Virtlogd_config<||> ~> Service['libvirt']
+    Virtlogd_config<||> ~> Service['virtlogd']
   }
 
   if ! $modular_libvirt {
@@ -168,7 +168,7 @@ class nova::compute::libvirt::services (
         name   => $virtsecret_service_name,
         tag    => ['libvirt-service', 'libvirt-modular-service'],
       }
-      Virtsecretd_config<||> ~> Service['virtlogd']
+      Virtsecretd_config<||> ~> Service['virtsecretd']
     }
 
     if $virtnodedev_service_name {
@@ -183,7 +183,7 @@ class nova::compute::libvirt::services (
         name   => $virtnodedev_service_name,
         tag    => ['libvirt-service', 'libvirt-modular-service'],
       }
-      Virtnodedevd_config<||> ~> Service['virtlogd']
+      Virtnodedevd_config<||> ~> Service['virtnodedevd']
     }
 
     if $virtqemu_service_name {
@@ -198,7 +198,7 @@ class nova::compute::libvirt::services (
         name   => $virtqemu_service_name,
         tag    => ['libvirt-service', 'libvirt-modular-service'],
       }
-      Virtqemud_config<||> ~> Service['virtlogd']
+      Virtqemud_config<||> ~> Service['virtqemud']
     }
 
     if $virtproxy_service_name {
@@ -208,7 +208,7 @@ class nova::compute::libvirt::services (
         name   => $virtproxy_service_name,
         tag    => ['libvirt-service', 'libvirt-modular-service'],
       }
-      Virtproxyd_config<||> ~> Service['virtlogd']
+      Virtproxyd_config<||> ~> Service['virtproxyd']
     }
 
     if $virtstorage_service_name {
@@ -223,7 +223,7 @@ class nova::compute::libvirt::services (
         name   => $virtstorage_service_name,
         tag    => ['libvirt-service', 'libvirt-modular-service'],
       }
-      Virtstoraged_config<||> ~> Service['virtlogd']
+      Virtstoraged_config<||> ~> Service['virtstoraged']
     }
   }
 }