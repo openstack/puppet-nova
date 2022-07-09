322599b75d59d59833b82772144424610e69aab3
Takashi Kajinami <tkajinam@redhat.com>
2022-07-09 13:11:43 +0900

    Switch provider to manage paste.ini file
    ... so that we users can use '<SERVICE DEFAULT>' similarly to
    the resource types to manage the <service>.conf files.
    
    Closes-Bug: #1981118
    Change-Id: I4092da4732d5e68ce190e7341df7a01166954504

--- lib/puppet/provider/nova_api_paste_ini/ini_setting.rb
+++ lib/puppet/provider/nova_api_paste_ini/ini_setting.rb
@@ -1,19 +1,7 @@
 Puppet::Type.type(:nova_api_paste_ini).provide(
   :ini_setting,
-  :parent => Puppet::Type.type(:ini_setting).provider(:ruby)
+  :parent => Puppet::Type.type(:openstack_config).provider(:ini_setting)
 ) do
-
-  def section
-    resource[:name].split('/', 2).first
-  end
-
-  def setting
-    resource[:name].split('/', 2).last
-  end
-
-  def separator
-    '='
-  end
 
   def self.file_path
     '/etc/nova/api-paste.ini'
