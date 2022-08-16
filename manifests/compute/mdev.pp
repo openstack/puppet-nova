f16181f25ca043c11aa47d6332d71a0da61cff27
Takashi Kajinami <tkajinam@redhat.com>
2022-08-16 23:11:37 +0900

    Remove deprecated mdev_types_device_addresses_mapping
    This parameter was deprecated during Yoga cycle in favor of the new
    mdev_types parameter[1].
    
    [1] 0be9d0a3c70001517bf2cf8535973bd77064ec96
    
    Change-Id: Idad6f62157ee637be64cee585beafe292efc9c15

--- manifests/compute/mdev.pp
+++ manifests/compute/mdev.pp
@@ -8,39 +8,14 @@
 #   (Optional) A hash to define the nova::compute::mdev_type resources.
 #   Defaults to {}
 #
-# DEPRECATED PARAMETERS
-#
-# [*mdev_types_device_addresses_mapping*]
-#   (Optional) Map of mdev type(s) the instances can get as key and list of
-#   corresponding device addresses as value.
-#   Defaults to undef
-#
 class nova::compute::mdev(
-  $mdev_types                          = {},
-  # DEPRECATED PARAMETERS
-  $mdev_types_device_addresses_mapping = undef,
+  $mdev_types = {},
 ) {
   include nova::deps
 
   validate_legacy(Hash, 'validate_hash', $mdev_types)
-  if $mdev_types_device_addresses_mapping != undef {
-    warning('mdev_types_device_addresses_mapping is deprecated. Use mdev_types.')
-    validate_legacy(Hash, 'validate_hash', $mdev_types_device_addresses_mapping)
-  }
 
-  $dev_addr_mapping_real = pick_default($mdev_types_device_addresses_mapping, {})
-
-  if !empty($dev_addr_mapping_real) {
-    nova_config {
-      'devices/enabled_mdev_types': value => join(keys($dev_addr_mapping_real), ',');
-    }
-
-    $dev_addr_mapping_real.each |$mdev_type, $device_addresses| {
-      nova::compute::mdev_type { $mdev_type :
-        device_addresses => $device_addresses;
-      }
-    }
-  } elsif !empty($mdev_types) {
+  if !empty($mdev_types) {
     nova_config {
       'devices/enabled_mdev_types': value => join(keys($mdev_types), ',')
     }
