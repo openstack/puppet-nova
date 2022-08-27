2c38b956543b343010521619eb12e7d3f6a93e88
Takashi Kajinami <tkajinam@redhat.com>
2022-08-28 00:47:29 +0900

    Use standard parameter description format for wsgi::apache
    Currently parameter description of the <module>::wsgi::apache classes
    are formatted differently in individual modules, and this is making
    the maintenance effort quite difficult.
    
    This change updates the description format following the standard one
    we are globally using in our modules to reduce undesired differences
    between modules.
    
    Change-Id: I1f78cc018c6a98412fdabb9aa731f1f7b8509eec

--- manifests/wsgi/apache_api.pp
+++ manifests/wsgi/apache_api.pp
@@ -15,109 +15,111 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 #
-# Class to serve Nova API and EC2 with apache mod_wsgi in place of nova-api and nova-api-ec2 services.
+# Class to serve Nova API with apache mod_wsgi in place of nova-api and service.
 #
-# Serving Nova API and Nova API EC2 from apache is the recommended way to go for production
-# because of limited performance for concurrent accesses.
+# Serving Nova API from apache is the recommended way to go for production
+# because of limited performance for concurrent accesses when running eventlet.
 #
-# When using this class you should disable your nova-api and nova-api-ec2 service.
+# When using this class you should disable your nova-api service.
 #
 # == Parameters
 #
-#   [*servername*]
-#     The servername for the virtualhost.
-#     Optional. Defaults to $::fqdn
+# [*servername*]
+#   (Optional) The servername for the virtualhost.
+#   Defaults to $::fqdn
 #
-#   [*api_port*]
-#     The port for Nova API service.
-#     Optional. Defaults to 8774
+# [*api_port*]
+#   (Optional) The port for Nova API service.
+#   Defaults to 8774
 #
-#   [*bind_host*]
-#     The host/ip address Apache will listen on.
-#     Optional. Defaults to undef (listen on all ip addresses).
+# [*bind_host*]
+#   (Optional) The host/ip address Apache will listen on.
+#   Defaults to undef (listen on all ip addresses).
 #
-#   [*path*]
-#     The prefix for the endpoint.
-#     Optional. Defaults to '/'
+# [*path*]
+#   (Optional) The prefix for the endpoint.
+#   Defaults to '/'
 #
-#   [*ssl*]
-#     Use ssl ? (boolean)
-#     Optional. Defaults to false
+# [*ssl*]
+#   (Optional) Use ssl ? (boolean)
+#   Defaults to false
 #
-#   [*workers*]
-#     Number of WSGI workers to spawn.
-#     Optional. Defaults to $::os_workers
+# [*workers*]
+#   (Optional) Number of WSGI workers to spawn.
+#   Defaults to $::os_workers
 #
-#   [*priority*]
-#     (optional) The priority for the vhost.
-#     Defaults to 10
+# [*priority*]
+#   (Optional) The priority for the vhost.
+#   Defaults to 10
 #
-#   [*threads*]
-#     (optional) The number of threads for the vhost.
-#     Defaults to 1
+# [*threads*]
+#   (Optional) The number of threads for the vhost.
+#   Defaults to 1
 #
-#   [*wsgi_process_display_name*]
-#     (optional) Name of the WSGI process display-name.
-#     Defaults to undef
+# [*wsgi_process_display_name*]
+#   (Optional) Name of the WSGI process display-name.
+#   Defaults to undef
 #
-#   [*ssl_cert*]
-#   [*ssl_key*]
-#   [*ssl_chain*]
-#   [*ssl_ca*]
-#   [*ssl_crl_path*]
-#   [*ssl_crl*]
-#   [*ssl_certs_dir*]
-#     apache::vhost ssl parameters.
-#     Optional. Default to apache::vhost 'ssl_*' defaults.
+# [*ssl_cert*]
+# [*ssl_key*]
+# [*ssl_chain*]
+# [*ssl_ca*]
+# [*ssl_crl_path*]
+# [*ssl_crl*]
+# [*ssl_certs_dir*]
+#   (Optional) apache::vhost ssl parameters.
+#   Default to apache::vhost 'ssl_*' defaults.
 #
-#   [*access_log_file*]
-#     The log file name for the virtualhost.
-#     Optional. Defaults to undef.
+# [*access_log_file*]
+#   (Optional) The log file name for the virtualhost.
+#   Defaults to undef.
 #
-#   [*access_log_pipe*]
-#     Specifies a pipe where Apache sends access logs for the virtualhost.
-#     Optional. Defaults to undef.
+# [*access_log_pipe*]
+#   (Optional) Specifies a pipe where Apache sends access logs for
+#   the virtualhost.
+#   Defaults to undef.
 #
-#   [*access_log_syslog*]
-#     Sends the virtualhost access log messages to syslog.
-#     Optional. Defaults to undef.
+# [*access_log_syslog*]
+#   (Optional) Sends the virtualhost access log messages to syslog.
+#   Defaults to undef.
 #
-#   [*access_log_format*]
-#     The log format for the virtualhost.
-#     Optional. Defaults to undef.
+# [*access_log_format*]
+#   (Optional) The log format for the virtualhost.
+#   Defaults to undef.
 #
-#   [*error_log_file*]
-#     The error log file name for the virtualhost.
-#     Optional. Defaults to undef.
+# [*error_log_file*]
+#   (Optional) The error log file name for the virtualhost.
+#   Defaults to undef.
 #
-#   [*error_log_pipe*]
-#     Specifies a pipe where Apache sends error logs for the virtualhost.
-#     Optional. Defaults to undef.
+# [*error_log_pipe*]
+#   (Optional) Specifies a pipe where Apache sends error logs for
+#   the virtualhost.
+#   Defaults to undef.
 #
-#   [*error_log_syslog*]
-#     Sends the virtualhost error log messages to syslog.
-#     Optional. Defaults to undef.
+# [*error_log_syslog*]
+#   (Optional) Sends the virtualhost error log messages to syslog.
+#   Defaults to undef.
 #
-#   [*custom_wsgi_process_options*]
-#     (optional) gives you the opportunity to add custom process options or to
-#     overwrite the default options for the WSGI main process.
-#     eg. to use a virtual python environment for the WSGI process
-#     you could set it to:
-#     { python-path => '/my/python/virtualenv' }
-#     Defaults to {}
+# [*custom_wsgi_process_options*]
+#   (Optional) gives you the opportunity to add custom process options or to
+#   overwrite the default options for the WSGI main process.
+#   eg. to use a virtual python environment for the WSGI process
+#   you could set it to:
+#   { python-path => '/my/python/virtualenv' }
+#   Defaults to {}
 #
-#   [*headers*]
-#     (optional) Headers for the vhost.
-#     Defaults to undef
+# [*headers*]
+#   (Optional) Headers for the vhost.
+#   Defaults to undef
 #
-#   [*request_headers*]
-#     (optional) Modifies collected request headers in various ways.
-#     Defaults to undef
+# [*request_headers*]
+#   (Optional) Modifies collected request headers in various ways.
+#   Defaults to undef
 #
-#   [*vhost_custom_fragment*]
-#     (optional) Passes a string of custom configuration
-#     directives to be placed at the end of the vhost configuration.
-#     Defaults to undef.
+# [*vhost_custom_fragment*]
+#   (Optional) Passes a string of custom configuration
+#   directives to be placed at the end of the vhost configuration.
+#   Defaults to undef.
 #
 # == Dependencies
 #
@@ -201,5 +203,4 @@ class nova::wsgi::apache_api (
     error_log_pipe              => $error_log_pipe,
     error_log_syslog            => $error_log_syslog,
   }
-
 }
--- manifests/wsgi/apache_metadata.pp
+++ manifests/wsgi/apache_metadata.pp
@@ -1,109 +1,129 @@
 # == Class: nova::wsgi::apache_metadata
 #
-# Class to serve Nova Metadata API with apache mod_wsgi in place of nova-metadata-api service.
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
 #
-# When using this class you should disable your nova-metadata-api_port service.
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Class to serve Nova Metadata API with apache mod_wsgi in place of
+# nova-metadata-api and service.
+#
+# Serving Nova Metadata API from apache is the recommended way to go for
+# production because of limited performance for concurrent accesses when
+# running eventlet.
+#
+# When using this class you should disable your nova-metadata-api service.
 #
 # == Parameters
 #
-#   [*servername*]
-#     The servername for the virtualhost.
-#     Optional. Defaults to $::fqdn
+# [*servername*]
+#   (Optional) The servername for the virtualhost.
+#   Defaults to $::fqdn
 #
-#   [*ensure_package*]
-#     (optional) Control the ensure parameter for the Nova Placement API package resource.
-#     Defaults to 'present'
+# [*ensure_package*]
+#   (Optional) Control the ensure parameter for the Nova Metadata API package
+#   resource.
+#   Defaults to 'present'
 #
-#   [*api_port*]
-#     The port for Nova API service.
-#     Optional. Defaults to 8775
+# [*api_port*]
+#   (Optional) The port for Nova API service.
+#   Defaults to 8775
 #
-#   [*bind_host*]
-#     The host/ip address Apache will listen on.
-#     Optional. Defaults to undef (listen on all ip addresses).
+# [*bind_host*]
+#   (Optional) The host/ip address Apache will listen on.
+#   Defaults to undef (listen on all ip addresses).
 #
-#   [*path*]
-#     The prefix for the endpoint.
-#     Optional. Defaults to '/'
+# [*path*]
+#   (Optional) The prefix for the endpoint.
+#   Defaults to '/'
 #
-#   [*ssl*]
-#     Use ssl ? (boolean)
-#     Optional. Defaults to false
+# [*ssl*]
+#   (Optional) Use ssl ? (boolean)
+#   Defaults to false
 #
-#   [*workers*]
-#     Number of WSGI workers to spawn.
-#     Optional. Defaults to $::os_workers
+# [*workers*]
+#   (Optional) Number of WSGI workers to spawn.
+#   Defaults to $::os_workers
 #
-#   [*priority*]
-#     (optional) The priority for the vhost.
-#     Defaults to 10
+# [*priority*]
+#   (Optional) The priority for the vhost.
+#   Defaults to 10
 #
-#   [*threads*]
-#     (optional) The number of threads for the vhost.
-#     Defaults to 1
+# [*threads*]
+#   (Optional) The number of threads for the vhost.
+#   Defaults to 1
 #
-#   [*wsgi_process_display_name*]
-#     (optional) Name of the WSGI process display-name.
-#     Defaults to undef
+# [*wsgi_process_display_name*]
+#   (Optional) Name of the WSGI process display-name.
+#   Defaults to undef
 #
-#   [*ssl_cert*]
-#   [*ssl_key*]
-#   [*ssl_chain*]
-#   [*ssl_ca*]
-#   [*ssl_crl_path*]
-#   [*ssl_crl*]
-#   [*ssl_certs_dir*]
-#     apache::vhost ssl parameters.
-#     Optional. Default to apache::vhost 'ssl_*' defaults.
+# [*ssl_cert*]
+# [*ssl_key*]
+# [*ssl_chain*]
+# [*ssl_ca*]
+# [*ssl_crl_path*]
+# [*ssl_crl*]
+# [*ssl_certs_dir*]
+#   (Optional) apache::vhost ssl parameters.
+#   Default to apache::vhost 'ssl_*' defaults.
 #
-#   [*access_log_file*]
-#     The log file name for the virtualhost.
-#     Optional. Defaults to undef.
+# [*access_log_file*]
+#   (Optional) The log file name for the virtualhost.
+#   Defaults to undef.
 #
-#   [*access_log_pipe*]
-#     Specifies a pipe where Apache sends access logs for the virtualhost.
-#     Optional. Defaults to undef.
+# [*access_log_pipe*]
+#   (Optional) Specifies a pipe where Apache sends access logs for
+#   the virtualhost.
+#   Defaults to undef.
 #
-#   [*access_log_syslog*]
-#     Sends the virtualhost access log messages to syslog.
-#     Optional. Defaults to undef.
+# [*access_log_syslog*]
+#   (Optional) Sends the virtualhost access log messages to syslog.
+#   Defaults to undef.
 #
-#   [*access_log_format*]
-#     The log format for the virtualhost.
-#     Optional. Defaults to undef.
+# [*access_log_format*]
+#   (Optional) The log format for the virtualhost.
+#   Defaults to undef.
 #
-#   [*error_log_file*]
-#     The error log file name for the virtualhost.
-#     Optional. Defaults to undef.
+# [*error_log_file*]
+#   (Optional) The error log file name for the virtualhost.
+#   Defaults to undef.
 #
-#   [*error_log_pipe*]
-#     Specifies a pipe where Apache sends error logs for the virtualhost.
-#     Optional. Defaults to undef.
+# [*error_log_pipe*]
+#   (Optional) Specifies a pipe where Apache sends error logs for
+#   the virtualhost.
+#   Defaults to undef.
 #
-#   [*error_log_syslog*]
-#     Sends the virtualhost error log messages to syslog.
-#     Optional. Defaults to undef.
+# [*error_log_syslog*]
+#   (Optional) Sends the virtualhost error log messages to syslog.
+#   Defaults to undef.
 #
-#   [*custom_wsgi_process_options*]
-#     (optional) gives you the opportunity to add custom process options or to
-#     overwrite the default options for the WSGI main process.
-#     eg. to use a virtual python environment for the WSGI process
-#     you could set it to:
-#     { python-path => '/my/python/virtualenv' }
-#     Defaults to {}
+# [*custom_wsgi_process_options*]
+#   (Optional) gives you the opportunity to add custom process options or to
+#   overwrite the default options for the WSGI main process.
+#   eg. to use a virtual python environment for the WSGI process
+#   you could set it to:
+#   { python-path => '/my/python/virtualenv' }
+#   Defaults to {}
 #
-#   [*headers*]
-#     (optional) Headers for the vhost.
-#     Defaults to undef
+# [*headers*]
+#   (Optional) Headers for the vhost.
+#   Defaults to undef
 #
-#   [*request_headers*]
-#     (optional) Modifies collected request headers in various ways.
-#     Defaults to undef
+# [*request_headers*]
+#   (Optional) Modifies collected request headers in various ways.
+#   Defaults to undef
 #
-#   [*vhost_custom_fragment*]
-#     (optional) Passes a string of custom configuration
-#     directives to be placed at the end of the vhost configuration.
-#     Defaults to undef.
+# [*vhost_custom_fragment*]
+#   (Optional) Passes a string of custom configuration
+#   directives to be placed at the end of the vhost configuration.
+#   Defaults to undef.
 #
 # == Dependencies
 #
@@ -196,5 +216,4 @@ class nova::wsgi::apache_metadata (
     error_log_pipe              => $error_log_pipe,
     error_log_syslog            => $error_log_syslog,
   }
-
 }
