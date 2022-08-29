db940667755e9e3ed4b309fbe0a12aec4c239c96
Bogdan Dobrelya <bdobreli@redhat.com>
2022-08-29 15:00:32 +0200

    Wire-in max_client_requests for libvirt
    Libvirt setting for (admin_/)max_client_requests is an important
    configuration to be tweaked when serving long running requests.
    
     Conflicts:
    	manifests/compute/libvirt/libvirtd.pp
    	spec/classes/nova_compute_libvirt_libvirtd_spec.rb
    
    Signed-off-by: Bogdan Dobrelya <bdobreli@redhat.com>
    Change-Id: I2d0ab2472f940ef6edac662e167033881ab09086
    (cherry picked from commit de886cd1dda892aa74ebeae00e0d17d391862f86)

--- manifests/compute/libvirt/virtqemud.pp
+++ manifests/compute/libvirt/virtqemud.pp
@@ -28,28 +28,41 @@
 #   on administrative socket.
 #   Defaults to $::os_service_default
 #
+# [*max_client_requests*]
+#   Limit on concurrent requests from a single client connection.
+#   Defaults to $::os_service_default
+#
+# [*admin_max_client_requests*]
+#   Limit on concurrent requests from a single client connection
+#   for the admin interface.
+#   Defaults to $::os_service_default
+#
 # [*ovs_timeout*]
 #   (optional) A timeout for openvswitch calls made by libvirt
 #   Defaults to $::os_service_default
 #
 #
 class nova::compute::libvirt::virtqemud (
-  $log_level         = $::os_service_default,
-  $log_filters       = $::os_service_default,
-  $log_outputs       = $::os_service_default,
-  $max_clients       = $::os_service_default,
-  $admin_max_clients = $::os_service_default,
-  $ovs_timeout       = $::os_service_default,
+  $log_level                 = $::os_service_default,
+  $log_filters               = $::os_service_default,
+  $log_outputs               = $::os_service_default,
+  $max_clients               = $::os_service_default,
+  $admin_max_clients         = $::os_service_default,
+  $max_client_requests       = $::os_service_default,
+  $admin_max_client_requests = $::os_service_default,
+  $ovs_timeout               = $::os_service_default,
 ) {
 
   include nova::deps
 
   virtqemud_config {
-    'log_level':         value => $log_level;
-    'log_filters':       value => $log_filters, quote => true;
-    'log_outputs':       value => $log_outputs, quote => true;
-    'max_clients':       value => $max_clients;
-    'admin_max_clients': value => $admin_max_clients;
-    'ovs_timeout':       value => $ovs_timeout;
+    'log_level':                 value => $log_level;
+    'log_filters':               value => $log_filters, quote => true;
+    'log_outputs':               value => $log_outputs, quote => true;
+    'max_clients':               value => $max_clients;
+    'admin_max_clients':         value => $admin_max_clients;
+    'max_client_requests':       value => $max_client_requests;
+    'admin_max_client_requests': value => $admin_max_client_requests;
+    'ovs_timeout':               value => $ovs_timeout;
   }
 }
