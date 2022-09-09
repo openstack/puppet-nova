75475c06bea454e3580ea6d83c88784fafb8912a
Takashi Kajinami <tkajinam@redhat.com>
2022-09-09 14:40:34 +0900

    Use the common provider for libvirt daemons
    Currently we implement separate providers for all libvirt configuration
    files but this is quite redundant because we have the same logic in
    all providers.
    
    This change refactors these providers makes use the libvirtd_config
    provider used as the base implementation.
    
    Change-Id: I486211306620d245262678875be3ec607640e921

--- lib/puppet/provider/virtlogd_config/ini_setting.rb
+++ lib/puppet/provider/virtlogd_config/ini_setting.rb
@@ -1,34 +1,7 @@
 Puppet::Type.type(:virtlogd_config).provide(
   :ini_setting,
-  :parent => Puppet::Type.type(:ini_setting).provider(:ruby)
+  :parent => Puppet::Type.type(:libvirtd_config).provider(:ini_setting)
 ) do
-
-  def exists?
-    if resource[:value] == ensure_absent_val
-      resource[:ensure] = :absent
-    elsif resource[:quote]
-      unless resource[:value].start_with?('"')
-        resource[:value] = '"' + resource[:value] + '"'
-      end
-    end
-    super
-  end
-
-  def section
-    ''
-  end
-
-  def setting
-    resource[:name]
-  end
-
-  def separator
-    '='
-  end
-
-  def ensure_absent_val
-    resource[:ensure_absent_val]
-  end
 
   def self.file_path
     '/etc/libvirt/virtlogd.conf'
