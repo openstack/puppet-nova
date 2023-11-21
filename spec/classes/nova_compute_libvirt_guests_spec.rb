411301564dac72b32d7c168ff81bcb7866873967
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-11-22 00:30:46 +0900

    Update libvirt-guests support
    This addresses several problems in the existing implementation.
    
    - The libvirt-guests service is available in Ubuntu and Debian.
      There is no specific reason to support only RHEL and CentOS.
    
    - The libvirt-guests package installs the client package in fact.
    
    - Updating libvirt client does not need restarting nova services.
    
    - Any change in config files should not restart the service. Restarting
      the libvirt-guests service shutdown all guests in that host.
    
    Change-Id: I2529925c439d7c22a6ada34e6da71bc29a79de12

--- spec/classes/nova_compute_libvirt_guests_spec.rb
+++ spec/classes/nova_compute_libvirt_guests_spec.rb
@@ -1,36 +1,36 @@
 require 'spec_helper'
-require 'puppet/util/package'
+
 describe 'nova::compute::libvirt_guests' do
 
-  shared_examples 'redhat-nova-compute-libvirt-guests' do
+  shared_examples 'nova::compute::libvirt_guests' do
 
     context 'with default parameters' do
 
       it { is_expected.to contain_class('nova::params')}
 
-      it { is_expected.to contain_file('/etc/sysconfig/libvirt-guests').with(
+      it { is_expected.to contain_file(platform_params[:libvirt_guests_environment_file]).with(
         :ensure => 'present',
-        :path   => '/etc/sysconfig/libvirt-guests',
+        :path   => platform_params[:libvirt_guests_environment_file],
         :tag    => 'libvirt-guests-file',
       ) }
-      it { is_expected.to contain_file_line('/etc/sysconfig/libvirt-guests ON_BOOT').with(
-        :path => '/etc/sysconfig/libvirt-guests',
+      it { is_expected.to contain_file_line('libvirt-guests ON_BOOT').with(
+        :path => platform_params[:libvirt_guests_environment_file],
         :line => 'ON_BOOT=ignore',
         :tag  => 'libvirt-guests-file_line'
       ) }
-      it { is_expected.to contain_file_line('/etc/sysconfig/libvirt-guests ON_SHUTDOWN').with(
-        :path => '/etc/sysconfig/libvirt-guests',
+      it { is_expected.to contain_file_line('libvirt-guests ON_SHUTDOWN').with(
+        :path => platform_params[:libvirt_guests_environment_file],
         :line => "ON_SHUTDOWN=shutdown",
         :tag  => 'libvirt-guests-file_line'
       ) }
-      it { is_expected.to contain_file_line('/etc/sysconfig/libvirt-guests SHUTDOWN_TIMEOUT').with(
-        :path => '/etc/sysconfig/libvirt-guests',
+      it { is_expected.to contain_file_line('libvirt-guests SHUTDOWN_TIMEOUT').with(
+        :path => platform_params[:libvirt_guests_environment_file],
         :line => "SHUTDOWN_TIMEOUT=300",
         :tag  => 'libvirt-guests-file_line'
       ) }
 
-      it { is_expected.to contain_package('libvirt-guests').with(
-        :name   => 'libvirt-client',
+      it { is_expected.to contain_package('libvirt-client').with(
+        :name   => platform_params[:libvirt_client_package_name],
         :ensure => 'present'
       ) }
 
@@ -39,16 +39,19 @@ describe 'nova::compute::libvirt_guests' do
 
     context 'with params' do
       let :params do
-        { :enabled=> true }
+        {
+          :enabled        => true,
+          :manage_service => true,
+        }
       end
 
-      it { is_expected.to contain_package('libvirt-guests').with(
-        :name   => 'libvirt-client',
+      it { is_expected.to contain_package('libvirt-client').with(
+        :name   => platform_params[:libvirt_client_package_name],
         :ensure => 'present'
       ) }
 
       it { is_expected.to contain_service('libvirt-guests').with(
-        :name   => 'libvirt-guests',
+        :name   => platform_params[:libvirt_guests_service_name],
         :enable => true,
         :ensure => 'running',
       )}
@@ -57,7 +60,7 @@ describe 'nova::compute::libvirt_guests' do
     context 'while not managing service state' do
       let :params do
         {
-          :manage_service => false,
+          :enabled => true,
         }
       end
 
@@ -70,12 +73,27 @@ describe 'nova::compute::libvirt_guests' do
     :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
-
-      case [:osfamily]
-      when 'RedHat'
-        it_behaves_like 'redhat-nova-compute-libvirt-guests'
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts({ :os_workers => 5 }))
       end
+
+      let (:platform_params) do
+        case facts[:os]['family']
+        when 'Debian'
+          {
+            :libvirt_client_package_name     => 'libvirt-clients',
+            :libvirt_guests_service_name     => 'libvirt-guests',
+            :libvirt_guests_environment_file => '/etc/default/libvirt-guests'
+          }
+        when 'RedHat'
+          {
+            :libvirt_client_package_name     => 'libvirt-client',
+            :libvirt_guests_service_name     => 'libvirt-guests',
+            :libvirt_guests_environment_file => '/etc/sysconfig/libvirt-guests'
+          }
+        end
+      end
+      it_behaves_like 'nova::compute::libvirt_guests'
     end
   end
-
 end
