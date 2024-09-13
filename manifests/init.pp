cd5c67af326f82e7cfd9f4c35e18d5ebcc5eaeb5
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-13 11:39:50 +0900

    Expose rabbit_qos_prefetch_count
    The option was supported by puppet-oslo but was not configurable via
    this module.
    
    Change-Id: Icd2a55375eec1c7c05a321091cfdf63b1121a0c2

--- manifests/init.pp
+++ manifests/init.pp
@@ -67,6 +67,10 @@
 #   will be run through a green thread.
 #   Defaults to $facts['os_service_default']
 #
+# [*rabbit_qos_prefetch_count*]
+#   (Optional) Specifies the number of messages to prefetch.
+#   Defaults to $facts['os_service_default']
+#
 # [*rabbit_quorum_queue*]
 #   (Optional) Use quorum queues in RabbitMQ.
 #   Defaults to $facts['os_service_default']
@@ -331,6 +335,7 @@ class nova(
   $rabbit_heartbeat_timeout_threshold     = $facts['os_service_default'],
   $rabbit_heartbeat_rate                  = $facts['os_service_default'],
   $rabbit_heartbeat_in_pthread            = $facts['os_service_default'],
+  $rabbit_qos_prefetch_count              = $facts['os_service_default'],
   $rabbit_ha_queues                       = $facts['os_service_default'],
   $rabbit_quorum_queue                    = $facts['os_service_default'],
   $rabbit_transient_quorum_queue          = $facts['os_service_default'],
@@ -504,6 +509,7 @@ but should be one of: ssh-rsa, ssh-dsa, ssh-ecdsa, ssh-ed25519.")
     heartbeat_timeout_threshold     => $rabbit_heartbeat_timeout_threshold,
     heartbeat_rate                  => $rabbit_heartbeat_rate,
     heartbeat_in_pthread            => $rabbit_heartbeat_in_pthread,
+    rabbit_qos_prefetch_count       => $rabbit_qos_prefetch_count,
     kombu_reconnect_delay           => $kombu_reconnect_delay,
     kombu_failover_strategy         => $kombu_failover_strategy,
     amqp_durable_queues             => $amqp_durable_queues,
