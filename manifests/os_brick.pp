de7b3a6aa39fb5baa283009916f62a26456344f3
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-25 02:26:26 +0900

    Support new os-brick options
    Add support for the new options added in os-brick 6.9.0 .
    
    Note:
    Release note is omitted because the os_brick class was added during
    this cycle.
    
    Depends-on: https://review.opendev.org/930364
    Change-Id: I4784f3c3f2870996d28d1b16261efc451c4a757e

--- manifests/os_brick.pp
+++ manifests/os_brick.pp
@@ -8,11 +8,24 @@
 #   (Optional) Directory to use for os-brick lock files.
 #   Defaults to $facts['os_service_default']
 #
+# [*wait_mpath_device_attempts*]
+#   (Optional) Number of attempts for the multipath device to be ready for I/O
+#   after it was created.
+#   Defaults to $facts['os_service_default']
+#
+# [*wait_mpath_device_interval*]
+#   (Optional) Interval value to wait for multipath device to be ready for I/O.
+#   Defaults to $facts['os_service_default']
+#
 class nova::os_brick(
-  $lock_path = $facts['os_service_default'],
+  $lock_path                  = $facts['os_service_default'],
+  $wait_mpath_device_attempts = $facts['os_service_default'],
+  $wait_mpath_device_interval = $facts['os_service_default'],
 ) {
 
   oslo::os_brick { 'nova_config':
-    lock_path => $lock_path
+    lock_path                  => $lock_path,
+    wait_mpath_device_attempts => $wait_mpath_device_attempts,
+    wait_mpath_device_interval => $wait_mpath_device_interval,
   }
 }
