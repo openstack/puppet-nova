39b08cc8a13c4315b45116768968f8b8883c8f0f
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-08-20 20:02:13 +0900

    Replace wsgi script
    pbr is removing support for wsgi_script due to changes in setuptools.
    Use the wsgi script module python code instead.
    
    Note that this change only touches CentOS code. Ubuntu and Debian will
    be updated later when we bump the release used in our testing.
    
    Change-Id: I5cd5c822b3078dbcc38fa4ebedcd4fbc591ce32b
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,6 +5,8 @@
 class nova::params {
   include openstacklib::defaults
 
+  $pyver3 = $openstacklib::defaults::pyver3
+
   $client_package = 'python3-novaclient'
   $user           = 'nova'
   $group          = 'nova'
@@ -64,8 +66,8 @@ class nova::params {
       $root_helper                       = 'sudo nova-rootwrap'
       $lock_path                         = '/var/lib/nova/tmp'
       $nova_wsgi_script_path             = '/var/www/cgi-bin/nova'
-      $nova_api_wsgi_script_source       = '/usr/bin/nova-api-wsgi'
-      $nova_metadata_wsgi_script_source  = '/usr/bin/nova-metadata-wsgi'
+      $nova_api_wsgi_script_source       = "/usr/lib/python${pyver3}/site-packages/nova/wsgi/osapi_compute.py"
+      $nova_metadata_wsgi_script_source  = "/usr/lib/python${pyver3}/site-packages/nova/wsgi/metadata.py"
     }
     'Debian': {
       # package names
