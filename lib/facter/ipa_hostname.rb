3eb363c3177279efc056cf862d46861887c924e9
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-09-20 11:25:07 +0900

    Remove ipa_hostname fact
    ... because it was used only by novajoin support which was removed
    multiple cycles ago.
    
    Change-Id: Icb80618901825b896e360872dbbf29685f896448
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- lib/facter/ipa_hostname.rb
+++
@@ -1,8 +0,0 @@
-Facter.add(:ipa_hostname) do
-  confine kernel: 'Linux'
-  setcode do
-    if File.exist?('/etc/ipa/default.conf')
-      Facter::Util::Resolution.exec('grep xmlrpc_uri /etc/ipa/default.conf | cut -d/ -f3')
-    end
-  end
-end
